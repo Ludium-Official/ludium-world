14bc789f233ae743df8579ab83e6cae731567c66
IDKNWHORU <junho9341@gmail.com>
2023-10-19 14:10:07 +0900

    백엔드 코드 추가

---
+++ back-end/src/test/java/world/ludium/education/EducationSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package world.ludium.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
