ce5eecdc0db5727ab9b4f7c8146c3906661025bb
IDKNWHORU <junho9341@gmail.com>
2023-10-19 14:59:02 +0900

    google 인증 후 페이지 이동하는 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 
 const CLIENT_ID = process.env.REACT_APP_OAUTH2_GOOGLE_CLIENT_ID;
@@ -5,13 +6,52 @@ const REDIRECT_URI = process.env.REACT_APP_OAUTH2_GOOGLE_REDIRECT_URI;
 const RESPONSE_TYPE = process.env.REACT_APP_OAUTH2_GOOGLE_RESPONSE_TYPE;
 const SCOPE = process.env.REACT_APP_OAUTH2_GOOGLE_SCOPE;
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="App">
+        <div className="ButtonArea">
+          <a className="LoginBtn" href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}>Google Login</a>
+        </div>
+      </div>
+    )
+  }, {
+    path: "/main",
+    element: (
+      <div className="App">
+        <div className="ButtonArea">
+          <Link className="ArticleBtn" to={`/article`}>아티클</Link>
+        </div>
+      </div >
+    )
+  }, {
+    path: "/article",
+    element: (
+      <div>
+        <button>
+        <Link to={`new`}>글쓰기</Link>
+        </button>
+        <div>
+          글 목록 표시
+        </div>
+      </div>
+    ),
+  },
+  {
+    path: "/article/new",
+    element: (
+      <div>
+        <button>업로드</button>
+        <label htmlFor="title">제목</label>
+        <input type="text" id="title" />
+      </div>
+    )
+  }
+])
+
 function App() {
-  return <div className="App">
-    <div className="ButtonArea">
-    <a className="LoginBtn" href={`https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}>Google Login</a>
-      <button className="ArticleBtn">articles</button>
-    </div>
-  </div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
