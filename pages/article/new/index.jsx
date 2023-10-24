9448e25003a9005f7a13a2f67603e85d3a8cedb7
junhojava <junho9341@gmail.com>
2023-10-24 18:08:41 +0900

    blocksuite editor의 데이터를 업로드하고 불러오는 기능 추가

--- pages/article/new/index.jsx
+++ pages/article/new/index.jsx
@@ -1,24 +1,70 @@
-import { useEffect, useRef } from "react"
-
-const getEditor = async (parent) => {
-  const { SimpleAffineEditor } = await import('@blocksuite/editor');
-  const editor = new SimpleAffineEditor();
-
-  parent.innerText = '';
-  parent.appendChild(editor);
-}
+import { useEffect, useRef, useState } from "react";
+import { AffineSchemas } from '@blocksuite/blocks/models';
+import { Schema, Workspace, Text } from '@blocksuite/store';
+import { IndexeddbPersistence } from 'y-indexeddb';
+import * as Y from 'yjs';
 
 export default function NewArticle() {
+  const schema = new Schema();
+  schema.register(AffineSchemas);
+  
   const divRef = useRef(null);
+  const [workspace, setWorkspace] = useState(null);
+  const [page, setPage] = useState(null);
+  
+  const getEditor = async (parent) => {
+    const { EditorContainer } = await import('@blocksuite/editor');
+    const workspace = new Workspace({ id: 'ludium', schema });
+    const page = workspace.createPage({ id: 'new-page' });
 
-  useEffect(_ => {
+    const editor = new EditorContainer();
+    editor.page = page;
+
+    page.waitForLoaded().then(() => {
+      const pageBlockId = page.addBlock('affine:page', { title: new Text('') });
+      const noteId = page.addBlock('affine:note', {}, pageBlockId);
+      page.addBlock('affine:paragraph', {}, noteId);
+    });
+
+    parent.innerText = '';
+    parent.appendChild(editor);
+
+    setWorkspace(workspace);
+    setPage(page);
+  }
+
+  const handleSave = _ => {
+    const binary = Y.encodeStateAsUpdate(workspace.doc);
+    const binaryString = Array.from(binary).map(byte => String.fromCharCode(byte)).join('');
+    localStorage.setItem('savedData', binaryString);
+  }
+
+  const handleLoad = _ => {
+    const binaryString = localStorage.getItem('savedData');
+    if (binaryString) {
+      const binary = new Uint8Array(binaryString.split('').map(char => char.charCodeAt(0)));
+      if (workspace) {
+        Y.applyUpdate(workspace.doc, binary);
+      }
+    } else {
+      console.error('No data found in localStorage');
+    }
+  }
+
+  const toggleReadOnly = _ => {
+    page.awarenessStore.setReadonly(page, !page.readonly);
+  }
+
+  useEffect(() => {
     getEditor(divRef.current)
   }, []);
 
   return (
     <>
-      <button>업로드</button>
-      <div ref={divRef}>로딩중</div>
+      <button onClick={handleSave}>Save</button>
+      <button onClick={handleLoad}>Load</button>
+      <button onClick={toggleReadOnly}>Toggle Read Only</button>
+      <div ref={divRef} style={{ width: '100%', height: '100vh' }}>Loading...</div>
     </>
   )
 }
