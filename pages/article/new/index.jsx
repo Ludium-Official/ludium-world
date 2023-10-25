1b999c2d013079f994800c8466f11b7c2a29cfec
IDKNWHORU <junho9341@gmail.com>
2023-10-25 14:00:39 +0900

    에디터를 tui-editor로 변경

--- pages/article/new/index.jsx
+++ pages/article/new/index.jsx
@@ -1,69 +1,38 @@
 import { useEffect, useRef, useState } from "react";
-import { AffineSchemas } from '@blocksuite/blocks/models';
-import { Schema, Workspace, Text } from '@blocksuite/store';
-import { IndexeddbPersistence } from 'y-indexeddb';
-import * as Y from 'yjs';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 export default function NewArticle() {
-  const schema = new Schema();
-  schema.register(AffineSchemas);
-  
+  const [editor, setEditor] = useState(null);
   const divRef = useRef(null);
-  const [workspace, setWorkspace] = useState(null);
-  const [page, setPage] = useState(null);
-  
-  const getEditor = async (parent) => {
-    const { EditorContainer } = await import('@blocksuite/editor');
-    const workspace = new Workspace({ id: 'ludium', schema });
-    const page = workspace.createPage({ id: 'new-page' });
-
-    const editor = new EditorContainer();
-    editor.page = page;
-
-    page.waitForLoaded().then(() => {
-      const pageBlockId = page.addBlock('affine:page', { title: new Text('') });
-      const noteId = page.addBlock('affine:note', {}, pageBlockId);
-      page.addBlock('affine:paragraph', {}, noteId);
-    });
-
-    parent.innerText = '';
-    parent.appendChild(editor);
-
-    setWorkspace(workspace);
-    setPage(page);
-  }
 
   const handleSave = _ => {
-    const binary = Y.encodeStateAsUpdate(workspace.doc);
-    const binaryString = Array.from(binary).map(byte => String.fromCharCode(byte)).join('');
-    localStorage.setItem('savedData', binaryString);
+    localStorage.setItem('md-content', editor.getMarkdown());
   }
 
   const handleLoad = _ => {
-    const binaryString = localStorage.getItem('savedData');
-    if (binaryString) {
-      const binary = new Uint8Array(binaryString.split('').map(char => char.charCodeAt(0)));
-      if (workspace) {
-        Y.applyUpdate(workspace.doc, binary);
-      }
-    } else {
-      console.error('No data found in localStorage');
-    }
-  }
-
-  const toggleReadOnly = _ => {
-    page.awarenessStore.setReadonly(page, !page.readonly);
+    editor.setMarkdown(localStorage.getItem('md-content'));
   }
 
   useEffect(() => {
-    getEditor(divRef.current)
-  }, []);
+    const putEditor = async () => {
+      const {Editor} = await import("@toast-ui/editor");
+
+      divRef.current.innerText = ''
+      setEditor(new Editor({
+        el: divRef.current,
+        height: '90vh',
+        initialEditType: 'wysiwyg',
+        previewStyle: 'tab',
+      }));
+    }
+
+    putEditor();
+  }, [])
 
   return (
     <>
       <button onClick={handleSave}>Save</button>
       <button onClick={handleLoad}>Load</button>
-      <button onClick={toggleReadOnly}>Toggle Read Only</button>
       <div ref={divRef} style={{ width: '100%', height: '100vh' }}>Loading...</div>
     </>
   )
