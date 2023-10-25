f50c96a6030fd02822a662b6e164a5d5f9a70312
IDKNWHORU <junho9341@gmail.com>
2023-10-25 17:48:59 +0900

    회원 가입 페이지 추가

--- pages/article/new/index.jsx
+++ pages/article/new/index.jsx
@@ -1,28 +1,28 @@
 import { useEffect, useRef, useState } from "react";
-import '@toast-ui/editor/dist/toastui-editor.css';
+import "@toast-ui/editor/dist/toastui-editor.css";
 
 export default function NewArticle() {
   const [editor, setEditor] = useState(null);
   const divRef = useRef(null);
 
   const handleSave = _ => {
-    localStorage.setItem('md-content', editor.getMarkdown());
+    localStorage.setItem("md-content", editor.getMarkdown());
   }
 
   const handleLoad = _ => {
-    editor.setMarkdown(localStorage.getItem('md-content'));
+    editor.setMarkdown(localStorage.getItem("md-content"));
   }
 
   useEffect(() => {
     const putEditor = async () => {
       const {Editor} = await import("@toast-ui/editor");
 
-      divRef.current.innerText = ''
+      divRef.current.innerText = ""
       setEditor(new Editor({
         el: divRef.current,
-        height: '90vh',
-        initialEditType: 'wysiwyg',
-        previewStyle: 'tab',
+        height: "90vh",
+        initialEditType: "wysiwyg",
+        previewStyle: "tab",
       }));
     }
 
@@ -33,7 +33,7 @@ export default function NewArticle() {
     <>
       <button onClick={handleSave}>Save</button>
       <button onClick={handleLoad}>Load</button>
-      <div ref={divRef} style={{ width: '100%', height: '100vh' }}>Loading...</div>
+      <div ref={divRef} style={{ width: "100%", height: "100vh" }}>Loading...</div>
     </>
   )
 }
