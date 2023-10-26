945d6e9b03fe74490bf0694efb60d96db06cdf53
IDKNWHORU <junho9341@gmail.com>
2023-10-26 15:44:27 +0900

    아티클 글쓰기 기능 추가

---
+++ components/Editor.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+export default function Editor({ editorRef }) {
+    useEffect(() => {
+        const putEditor = async () => {
+            const toastEditor = await import("@toast-ui/editor");
+
+            try {
+                editorRef.current.innerText = "";
+
+                editorRef.current.editorInstance = new toastEditor.Editor({
+                    el: editorRef.current,
+                    height: "90vh",
+                    initialEditType: "wysiwyg",
+                    previewStyle: "tab",
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        putEditor();
+    }, []);
+
+    return (
+        <>
+            <div ref={editorRef} style={{ width: "100%", height: "100vh" }}>Loading...</div>
+        </>
+    );
+}
