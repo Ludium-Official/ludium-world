7799ef19d949a5fb10e501b772970f09e22b5dd6
IDKNWHORU <junho9341@gmail.com>
2023-10-26 15:44:27 +0900

    아티클 글쓰기 기능 추가

--- pages/main/index.jsx
+++ pages/main/index.jsx
@@ -2,23 +2,5 @@ import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Main() {
-  useEffect(_ => {
-    const userInfo = async () => {
-      const { value } = await cookieStore.get("access_token");
-
-      const info = await fetch("http://localhost:8080/auth/google/info/google", {
-        headers: {
-          Authorization: `Bearer${value}`
-        }
-      })
-
-      const {email, given_name, id, name} = await info.json();
-
-      console.log({ email, given_name, id, name });
-    }
-
-    userInfo();
-  }, []);
-
   return <Link href={"/article"}>아티클</ Link>
 }
