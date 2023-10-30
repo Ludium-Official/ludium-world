b714fcea93cb6e072cc05d24c3526a036163c6b8
IDKNWHORU <junho9341@gmail.com>
2023-10-30 16:12:20 +0900

    자유 게시판 추가

---
+++ pages/post/new/index.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from "react";
+import Editor from "../../../components/Editor";
+import { useRouter } from "next/router";
+
+export default function NewPost() {
+  const router = useRouter();
+  const titleRef = useRef(null);
+  const editorRef = useRef(null);
+
+  const handleSave = async () => {
+    const { editorInstance } = editorRef.current;
+    const formData = new FormData();
+
+    formData.append("title", titleRef.current.value);
+    formData.append("content", editorInstance.getMarkdown());
+
+    const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
+    const createPostResponse = await fetch(`${serverUri}/post`, {
+      method: "post",
+      body: formData,
+      credentials: "include"
+    });
+
+    if (createPostResponse.ok) {
+      router.push("/post");
+    }
+  }
+
+  return (
+    <>
+      <button onClick={handleSave}>저장</button>
+      <input type="text" name="title" id="title" ref={titleRef} placeholder="제목을 입력해주세요" />
+      <Editor editorRef={editorRef} />
+    </>
+  )
+}
