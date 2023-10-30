e7197d6dff97e56b14cfad9b9560a1e5ebe88e13
IDKNWHORU <junho9341@gmail.com>
2023-10-30 15:27:47 +0900

    미션 검증 기능 추가

--- src/main/java/world/ludium/education/mission/MissionSubmitRepository.java
+++ src/main/java/world/ludium/education/mission/MissionSubmitRepository.java
@@ -2,8 +2,9 @@ package world.ludium.education.mission;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MissionSubmitRepository extends JpaRepository<MissionSubmit, UUID> {
-
+    List<MissionSubmit> findAllByMsnId(UUID msnId);
 }
--- src/main/java/world/ludium/education/mission/MissionSubmitService.java
+++ src/main/java/world/ludium/education/mission/MissionSubmitService.java
@@ -1,8 +1,10 @@
 package world.ludium.education.mission;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -29,5 +31,18 @@ public class MissionSubmitService {
         missionSubmitHistory.setCreateAt(ZonedDateTime.now());
 
         return missionSubmitHistoryRepository.save(missionSubmitHistory);
+    }
+
+    public List<MissionSubmit> getMissionSubmits(UUID missionId) {
+        return missionSubmitRepository.findAllByMsnId(missionId);
+    }
+
+    public MissionSubmit validateMissionSubmit(UUID submitId) {
+        MissionSubmit missionSubmit = missionSubmitRepository.findById(submitId)
+                .orElseThrow(() -> new EntityNotFoundException("미션 서브밋을 찾을 수 없습니다. ID: " + submitId));
+
+        missionSubmit.setVldStt(true);
+
+        return missionSubmitRepository.save(missionSubmit);
     }
 }
