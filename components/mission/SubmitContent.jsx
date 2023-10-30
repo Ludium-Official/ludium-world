17ad6b40782c9267eff7961f2d715e2059a47050
IDKNWHORU <junho9341@gmail.com>
2023-10-30 16:16:18 +0900

    검증 해제 기능 추가

--- components/mission/SubmitContent.jsx
+++ components/mission/SubmitContent.jsx
@@ -1,13 +1,13 @@
 import { useRef, useState } from "react";
 import Viewer from "../Viewer";
 
-export default function SubmitContent ({id, content, vldStt}) {
+export default function SubmitContent({ id, content, vldStt }) {
     const [submitData, setSubmitData] = useState({
         content: content,
         isValidate: vldStt
     })
     const viewerRef = useRef(null);
-    
+
     const handleValidate = async (submitId) => {
         const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
 
@@ -16,7 +16,7 @@ export default function SubmitContent ({id, content, vldStt}) {
             credentials: "include"
         });
 
-        if(validateMissionSubmitResponse.ok) {
+        if (validateMissionSubmitResponse.ok) {
             setSubmitData({
                 ...submitData,
                 isValidate: true
@@ -24,11 +24,35 @@ export default function SubmitContent ({id, content, vldStt}) {
         }
     }
 
+    const handleInvalidate = async (submitId) => {
+        const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
+
+        const invalidateMissionSubmitResponse = await fetch(`${serverUri}/mission/submit/invalidate/${submitId}`, {
+            method: "put",
+            credentials: "include"
+        });
+
+        if (invalidateMissionSubmitResponse.ok) {
+            setSubmitData({
+                ...submitData,
+                isValidate: false
+            });
+        }
+    }
+
     return (
         <div key={id} style={{ display: "flex", justifyContent: "space-between" }}>
-          <Viewer viewerRef={viewerRef} content={submitData.content} />
-          <div>{submitData.isValidate ? "검증O" : "검증X"}</div>
-          <button onClick={() => handleValidate(id)}>검증</button>
+            <Viewer viewerRef={viewerRef} content={submitData.content} />
+            {submitData.isValidate ?
+                <>
+                    <p>검증O</p>
+                    <button onClick={() => handleInvalidate(id)}>검증해제</button>
+                </> :
+                <>
+                    <p>검증X</p>
+                    <button onClick={() => handleValidate(id)}>검증</button>
+                </>
+            }
         </div>
-      );
+    );
 }
