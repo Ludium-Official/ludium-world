92fcdfcd961d604a2a7f4c16db5ee09f62d2c782
IDKNWHORU <junho9341@gmail.com>
2023-10-31 17:03:19 +0900

    미션 제출 댓글 기능 추가

--- pages/mission/[missionId]/submit/index.jsx
+++ pages/mission/[missionId]/submit/index.jsx
@@ -14,6 +14,15 @@ export default function GetSubmits() {
 
     return <>
         <h1>미션 제출 목록</h1>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <p>내용</p>
+            <p>작성자</p>
+            <p>검증여부</p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
         <hr />
         <MissionSubmit missionId={router.query.missionId} />
     </>
