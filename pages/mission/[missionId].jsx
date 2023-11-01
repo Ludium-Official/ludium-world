64cad95533045ee555fdcca252233c0600dcc7e2
junhojava <junho9341@gmail.com>
2023-11-01 16:15:59 +0900

    미션 데이터를 서버에서 불러오도록 변경

--- pages/mission/[missionId].jsx
+++ pages/mission/[missionId].jsx
@@ -1,60 +1,46 @@
 import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import Viewer from "../../components/Viewer";
 
 export async function getServerSideProps(context) {
+    const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
+    const { missionId } = context.query;
+
+    const getMissionResponse = await fetch(`${serverUri}/mission/${missionId}`);
+
+    if (!getMissionResponse.ok) {
+        return {
+            props: {
+                missions: null,
+                missionId
+            }
+        };
+    }
+
     return {
-        props: {}
+        props: {
+            mission: await getMissionResponse.json(),
+            missionId
+        }
     };
 }
 
-export default function GetMission() {
+export default function GetMission({ mission, missionId }) {
     const router = useRouter();
     const viewerRef = useRef(null);
-    const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
-    const [mission, setMission] = useState({ id: null, title: "", content: "" });
 
     const handleGoSubmit = () => {
-        router.push(`/mission/${router.query.missionId}/submit/new`);
+        router.push(`/mission/${missionId}/submit/new`);
     }
 
     const handleGoMissionSubmit = () => {
-        router.push(`/mission/${router.query.missionId}/submit`);
+        router.push(`/mission/${missionId}/submit`);
     }
-
-    useEffect(() => {
-        const getMission = async (missionId) => {
-            if (missionId) {
-                const getMissionResponse = await fetch(`${serverUri}/mission/${missionId}`);
-
-                if (getMissionResponse.ok) {
-
-                    const mission = await getMissionResponse.json();
-                    setMission(mission);
-
-                    if (viewerRef.current.viewerInstance === undefined) {
-                        setTimeout(ref => {
-                            if (ref.current === null) return;
-                            const viewerInstance = ref.current.viewerInstance;
-
-                            viewerInstance.setMarkdown(mission.content);
-                        }, 1000, viewerRef);
-                    } else {
-                        const viewerInstance = viewerRef.current.viewerInstance;
-
-                        viewerInstance.setMarkdown(mission.content);
-                    }
-                }
-            }
-        }
-
-        getMission(router.query.missionId);
-    }, []);
 
     return <>
         <button onClick={handleGoSubmit}>미션제출하러가기</button>
         <button onClick={handleGoMissionSubmit}>미션 제출 보기</button>
         <input type="text" name="title" id="title" readOnly={true} value={mission.title} />
-        <Viewer viewerRef={viewerRef} />
+        <Viewer viewerRef={viewerRef} content={mission.content} />
     </>
 }
