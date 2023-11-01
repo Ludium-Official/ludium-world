1756d20090b9d7aba599f57a4993f203afb3cceb
junhojava <junho9341@gmail.com>
2023-11-01 16:32:42 +0900

    미션 제출 목록을 서버에서 불러오도록 변경

--- front-end/pages/mission/[missionId]/submit/[submitId]/history.jsx
+++ front-end/pages/mission/[missionId]/submit/[submitId]/history.jsx
@@ -1,39 +1,34 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import SubmitHistory from "../../../../../components/mission/SubmitHistory";
 import Link from "next/link";
+import SubmitHistory from "../../../../../components/mission/SubmitHistory";
 
 export async function getServerSideProps(context) {
+    const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
+    const { missionId, submitId } = context.query;
+
+    const getSubmitHistoryResponse = await fetch(`${serverUri}/mission/${missionId}/submit/${submitId}/history`);
+
+    if (!getSubmitHistoryResponse.ok) {
+        return {
+            props: {
+                history: [],
+                missionId
+            }
+        };
+    }
+
     return {
-        props: {}
+        props: {
+            history: await getSubmitHistoryResponse.json(),
+            missionId
+        }
     };
 }
 
-export default function HistorySubmit() {
-    const router = useRouter();
-    const [submitHistory, setSubmitHistory] = useState([]);
-
-    const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
-    const { missionId, submitId } = router.query;
-
-    useEffect(() => {
-        const getSubmitHistory = async () => {
-            const getSubmitHistoryResponse = await fetch(`${serverUri}/mission/${missionId}/submit/${submitId}/history`);
-
-            if (getSubmitHistoryResponse.ok) {
-                setSubmitHistory(await getSubmitHistoryResponse.json());
-            }
-        }
-
-        getSubmitHistory();
-    }, []);
-
-    return (
-        <>
-            <Link href="../" as={`/mission/${missionId}/submit`}>미션 제출 목록으로 가기</Link>
-            {submitHistory.map((history) => (
-                <SubmitHistory key={history.id} {...history}/>
-            ))}
-        </>
-    )
+export default function HistorySubmit({ history, missionId }) {
+    return <>
+        <Link href="../" as={`/mission/${missionId}/submit`}>미션 제출 목록으로 가기</Link>
+        {history.map((item) => (
+            <SubmitHistory key={item.id} {...item} />
+        ))}
+    </>
 }
