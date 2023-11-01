b7b7c2c4a27b7eefc74ca9980902d654421f6d3e
junhojava <junho9341@gmail.com>
2023-11-01 17:22:57 +0900

    자유 게시글 목록을 서버에서 불러오도록 변경

--- pages/post/index.jsx
+++ pages/post/index.jsx
@@ -1,27 +1,31 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
-export default function Post() {
-  const [posts, setPosts] = useState([]);
+export async function getServerSideProps() {
+  const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
+  const getPostsResponse = await fetch(`${serverUri}/post`);
 
-  useEffect(() => {
-    const getPosts = async () => {
-      const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
-      const getPostsResponse = await fetch(`${serverUri}/post`);
-
-      if (getPostsResponse.ok) {
-        setPosts(await getPostsResponse.json());
+  if (!getPostsResponse.ok) {
+    return {
+      props: {
+        posts: null
       }
     }
+  }
 
-    getPosts();
-  }, [])
+  return {
+    props: {
+      posts: await getPostsResponse.json()
+    }
+  };
+}
+
+export default function Posts({ posts }) {
 
   return (
     <>
       <Link href="/post/new">글쓰기</Link>
       <h1>글 목록</h1>
-      <div style={{display: "flex", flexDirection: "column"}}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
         {posts.map(post => (
           <Link key={post.id} href={`/post/${post.id}`}>{post.title}</Link>
         ))}
