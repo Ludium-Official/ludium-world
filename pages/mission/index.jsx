e47a6ced1550efbf51fd17f84b1b85de36e063e0
junhojava <junho9341@gmail.com>
2023-11-01 16:09:50 +0900

    미션 목록을 서버에서 불러오도록 변경

--- pages/mission/index.jsx
+++ pages/mission/index.jsx
@@ -1,31 +1,33 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
-export default function Mission() {
-  const [missions, setMissions] = useState([]);
+export async function getServerSideProps() {
+  const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
+  const getMissionsResponse = await fetch(`${serverUri}/mission`);
 
-  useEffect(() => {
-    const getMissions = async () => {
-      const serverUri = process.env.NEXT_PUBLIC_BACKEND_URI;
-      const getMissionsResponse = await fetch(`${serverUri}/mission`);
-
-      if (getMissionsResponse.ok) {
-        setMissions(await getMissionsResponse.json());
+  if (!getMissionsResponse.ok) {
+    return {
+      props: {
+        missions: []
       }
     }
+  }
 
-    getMissions();
-  }, [])
+  return {
+    props: {
+      missions: await getMissionsResponse.json()
+    }
+  };
+}
 
-  return (
-    <>
-      <Link href="/mission/new">글쓰기</Link>
-      <h1>글 목록</h1>
-      <div style={{display: "flex", flexDirection: "column"}}>
-        {missions.map(mission => (
-          <Link key={mission.id} href={`/mission/${mission.id}`}>{mission.title}</Link>
-        ))}
-      </div>
-    </>
-  )
+export default function Mission({ missions }) {
+
+  return <>
+    <Link href="/mission/new">글쓰기</Link>
+    <h1>글 목록</h1>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {missions.map(mission => (
+        <Link key={mission.id} href={`/mission/${mission.id}`}>{mission.title}</Link>
+      ))}
+    </div>
+  </>
 }
