282ce86e6c39a63728266c3d135c33ec201ef185
junhojava <junho9341@gmail.com>
2023-11-03 18:28:58 +0900

    내 정보 수정 기능 추가

--- pages/sign-up/index.jsx
+++ pages/sign-up/index.jsx
@@ -9,7 +9,7 @@ export default function SignUP() {
         e.preventDefault();
 
         const formData = new FormData(e.target);
-        
+
         const { editorInstance } = editorRef.current;
         formData.append("self_intro", editorInstance.getMarkdown());
 
@@ -18,15 +18,15 @@ export default function SignUP() {
             const res = await fetch(`${serverUri}/user/sign-up/google`, {
                 method: "post",
                 body: formData,
-                credentials : "include"
+                credentials: "include"
             })
 
-            if(res.ok) {
+            if (res.ok) {
                 router.push("/main");
             }
-            
+
         } catch (error) {
-            
+
         }
     }
     return (
@@ -34,8 +34,8 @@ export default function SignUP() {
             <label htmlFor="nick">닉네임</label>
             <input type="text" name="nick" id="nick" placeholder="닉네임을 입력하세요" />
             <label htmlFor="phone_number">핸드폰번호</label>
-            <input type="number" name="phone_number" id="phone_number" placeholder="휴대폰번호를 입력해주세요" />
-            <label htmlFor="self_intro">자기소개</label>
+            <input type="number" name="phone_number" id="phone_number" placeholder="'-' 없이 숫자만 입력해주세요." />
+            <p>자기소개</p>
             <Editor editorRef={editorRef} />
             <button>취소하기</button>
             <input type="submit" value="저장하기" />
