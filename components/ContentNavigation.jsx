8f0191c1ce73b13ac5171f99fbb89633f54bb488
IDKNWHORU <junho9341@gmail.com>
2023-11-21 16:28:07 +0900

    미션 목록 스타일 추가

---
+++ components/ContentNavigation.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import contentnavigationstyle from "./ContentNavigation.module.css";
+
+export default function ContentNavigation({links}) {
+    return <nav className={contentnavigationstyle.wrapper}>
+        <ul className={contentnavigationstyle.list}>
+            {links.map(link => <li>
+                <Link href={link.href}>{link.text}</Link>
+            </li> )}
+        </ul>
+    </nav>
+}
