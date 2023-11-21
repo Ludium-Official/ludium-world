6b7c1a074d68ed1d275291761570c65caccbfc28
IDKNWHORU <junho9341@gmail.com>
2023-11-21 16:45:44 +0900

    미션 제출 보기 페이지 스타일 추가

---
+++ front-end/app/mission/[missionId]/submit/page.jsx
@@ -0,0 +1,33 @@
+import SubmitContent from "../../../../components/mission/SubmitContent";
+import fetchWithRetry from "../../../../functions/api";
+import submitstyle from "./submit.module.css";
+
+async function getSubmitList(missionId) {
+    const getMissionSumitResponse = await fetchWithRetry(`/mission/${missionId}/submit`);
+
+    if (!getMissionSumitResponse.ok) return [];
+
+    return await getMissionSumitResponse.json();
+}
+
+export default async function MissionSubmitListPage({ params }) {
+    const { missionId } = params;
+    const submitList = await getSubmitList(missionId);
+
+    return <article className={submitstyle.wrapper}>
+        <h1>미션 제출 목록</h1>
+        <section className={submitstyle["section-header-wrapper"]}>
+            <p>내용</p>
+            <p>작성자</p>
+            <p>검증 여부</p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </section>
+        {
+            submitList.map(submit =>
+                <SubmitContent key={submit.id} missionId={missionId} {...submit} />)
+        }
+    </article>
+}
--- front-end/components/mission/SubmitContent.jsx
+++ front-end/components/mission/SubmitContent.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { useRef, useState } from "react";
 import Viewer from "../Viewer";
