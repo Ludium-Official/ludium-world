4701ff5ed0669b3fb3abf9da2490ad6bcdc5de49
junhojava <junho9341@gmail.com>
2023-11-24 00:25:48 +0900

    교육 페이지 스타일 추가

---
+++ app/course/[courseId]/page.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Viewer from "../../../components/Viewer";
+import fetchWithRetry from "../../../functions/api";
+
+async function getCourse(courseId) {
+    const getCourseResponse = await fetchWithRetry(`/course/${courseId}`);
+
+    if (!getCourseResponse.ok) return null;
+
+    return await getCourseResponse.json();
+}
+
+export default async function CoursePage({ params: {courseId} }) {
+    const course = await getCourse(courseId);
+
+    return <>
+        <input type="text" defaultValue={course.title} readOnly />
+        <h1>모듈 목록</h1>
+        {course.modules.map(module => <div key={module.id}>
+            <Link href={`/course/${courseId}/module/${module.id}`}>{module.title}</Link>
+        </div>)}
+        <hr />
+        <Viewer content={course.content} />
+    </>;
+}
