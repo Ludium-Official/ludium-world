1cc301eef64f5428aa134ed30aba3ac393f6875c
junhojava <junho9341@gmail.com>
2023-11-24 17:19:54 +0900

    미션 추가 페이지 폼 요소 스타일 추가

--- app/mission/new/page.jsx
+++ app/mission/new/page.jsx
@@ -5,37 +5,58 @@ import { useRef } from "react";
 import Editor from "../../../components/Editor";
 import Category from "../../../enums/Category";
 import fetchWithRetry from "../../../functions/api";
+import missionstyle from "../mission.module.css";
 
 export default function NewMissionPage() {
-    const router = useRouter();
-    const editorRef = useRef(null);
+  const router = useRouter();
+  const editorRef = useRef(null);
 
-    const handleSaveMission = async (e) => {
-        e.preventDefault();
+  const handleSaveMission = async (e) => {
+    e.preventDefault();
 
-        const { editorInstance } = editorRef.current;
-        const formData = new FormData(e.target);
+    const { editorInstance } = editorRef.current;
+    const formData = new FormData(e.target);
 
-        formData.append("content", editorInstance.getMarkdown());
+    formData.append("content", editorInstance.getMarkdown());
 
-        const createMissionResponse = await fetchWithRetry(`/article`, {
-            method: "POST",
-            body: formData,
-        });
+    const createMissionResponse = await fetchWithRetry(`/article`, {
+      method: "POST",
+      body: formData,
+    });
 
-        if (createMissionResponse.ok) {
-            router.push("/mission");
-            router.refresh();
-        }
+    if (createMissionResponse.ok) {
+      router.push("/mission");
+      router.refresh();
     }
+  };
 
-    return <form onSubmit={handleSaveMission}>
-        <input type="text" name="title" id="title" placeholder="제목을 입력해주세요" />
+  return (
+    <form
+      className={missionstyle["mission-new-wrapper"]}
+      onSubmit={handleSaveMission}
+    >
+      <div className={missionstyle["mission-new-button-area"]}>
+        <input
+          className={missionstyle["mission-new-button"]}
+          type="submit"
+          value="저장하기"
+        />
+      </div>
+      <div className={missionstyle["mission-new-header-area"]}>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="제목을 입력해주세요"
+        />
         <select name="category" id="category">
-            <option value={Category.MISSION}>미션</option>
-            <option value={Category.ARTICLE}>아티클</option>
+          <option value={Category.MISSION}>미션</option>
+          <option value={Category.ARTICLE}>아티클</option>
         </select>
-        <input type="submit" value="저장하기" />
-        <Editor editorRef={editorRef} />
+      </div>
+      <div className={missionstyle["mission-new-content-area"]}>
+        <Editor editorRef={editorRef} height={"100%"} />
+      </div>
     </form>
+  );
 }
