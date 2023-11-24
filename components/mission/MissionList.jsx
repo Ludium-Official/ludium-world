f21ec0abdeacbc829ce4fa43c2ea664669dbcd37
junhojava <junho9341@gmail.com>
2023-11-24 15:48:33 +0900

    미션 타일 콘텐츠 스타일 추가

--- components/mission/MissionList.jsx
+++ components/mission/MissionList.jsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
-import missionstyle from "./Mission.module.css";
+import missionliststyle from "./MissionList.module.css";
+import Viewer from "../Viewer";
 
 export default function MissionList({ missions }) {
   return (
-    <div className={missionstyle["mission-list-wrapper"]}>
+    <div className={missionliststyle["mission-list-wrapper"]}>
       {missions.map(({ id, title, content, usrId }) => (
-        <Link key={id} href={`/mission/${id}`} className={missionstyle["mission-link-wrapper"]}>
-          <section className={missionstyle["mission-wrapper"]}>
-            <h1 className={missionstyle["mission-title"]}>{title}</h1>
-            <h2 className={missionstyle["mission-author"]}>{usrId}</h2>
-            <p className={missionstyle["mission-content"]}>{content}</p>
+        <Link
+          key={id}
+          href={`/mission/${id}`}
+          className={missionliststyle["mission-link-wrapper"]}
+        >
+          <section className={missionliststyle["mission-wrapper"]}>
+            <h1 className={missionliststyle["mission-title"]}>{title}</h1>
+            <h2 className={missionliststyle["mission-author"]}>{usrId}</h2>
+            <div className={missionliststyle["mission-content"]}>
+              <Viewer content={content}></Viewer>
+            </div>
           </section>
         </Link>
       ))}
