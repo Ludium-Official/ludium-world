a2953c4c7f46034d06da89daad7b733d0cf77afd
junhojava <junho9341@gmail.com>
2023-11-25 15:23:35 +0900

    미션 제출 목록 스타일 추가

--- front-end/app/mission/[missionId]/submit/page.jsx
+++ front-end/app/mission/[missionId]/submit/page.jsx
@@ -1,33 +1,35 @@
 import SubmitContent from "../../../../components/mission/SubmitContent";
 import fetchWithRetry from "../../../../functions/api";
-import submitstyle from "../../mission.module.css"
+import submitstyle from "../../mission.module.css";
 
 async function getSubmitList(missionId) {
-    const getMissionSumitResponse = await fetchWithRetry(`/mission/${missionId}/submit`);
+  const getMissionSumitResponse = await fetchWithRetry(
+    `/mission/${missionId}/submit`
+  );
 
-    if (!getMissionSumitResponse.ok) return [];
+  if (!getMissionSumitResponse.ok) return [];
 
-    return await getMissionSumitResponse.json();
+  return await getMissionSumitResponse.json();
 }
 
-export default async function MissionSubmitListPage({ params }) {
-    const { missionId } = params;
-    const submitList = await getSubmitList(missionId);
+export default async function MissionSubmitListPage({ params: { missionId } }) {
+  const submitList = await getSubmitList(missionId);
 
-    return <>
-        <h1>미션 제출 목록</h1>
-        <section className={submitstyle["section-header-wrapper"]}>
-            <p>내용</p>
-            <p>작성자</p>
-            <p>검증 여부</p>
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
-        </section>
-        {
-            submitList.map(submit =>
-                <SubmitContent key={submit.id} missionId={missionId} {...submit} />)
-        }
-    </>
+  return (
+    <div className={submitstyle["mission-submit-wrapper"]}>
+      <h1>미션 제출 목록</h1>
+      <article className={submitstyle["mission-submit-list"]}>
+        <p className={submitstyle["mission-submit-item"]}>내용</p>
+        <p className={submitstyle["mission-submit-item"]}>작성자</p>
+        <p className={submitstyle["mission-submit-item"]}>검증 여부</p>
+        <p className={submitstyle["mission-submit-item"]}></p>
+        <p className={submitstyle["mission-submit-item"]}></p>
+        <p className={submitstyle["mission-submit-item"]}></p>
+        <p className={submitstyle["mission-submit-item"]}></p>
+        {submitList.map((submit) => (
+          <SubmitContent key={submit.id} missionId={missionId} {...submit} />
+        ))}
+      </article>
+    </div>
+  );
 }
