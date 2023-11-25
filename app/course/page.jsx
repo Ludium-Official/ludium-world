3d1edd34b8df01427576fbffb16d277e71bcc5ff
junhojava <junho9341@gmail.com>
2023-11-25 15:48:00 +0900

    미션 수정 페이지 폼 스타일 추가

--- app/course/page.jsx
+++ app/course/page.jsx
@@ -1,24 +1,38 @@
 import Link from "next/link";
 import fetchWithRetry from "../../functions/api";
+import coursestyle from "./course.module.css";
+import ContentNavigation from "../../components/ContentNavigation";
 
 async function getCourseList() {
-    const getCoursesResponse = await fetchWithRetry(`/course`);
+  const getCoursesResponse = await fetchWithRetry(`/course`);
 
-    if (!getCoursesResponse.ok) return [];
+  if (!getCoursesResponse.ok) return [];
 
-    return await getCoursesResponse.json();
+  return await getCoursesResponse.json();
 }
 
 export default async function CoursePage() {
-    const courses = await getCourseList();
+  const courses = await getCourseList();
+  const courseLinks = [
+    {
+      href: "/course/new",
+      text: "교육 만들기",
+    },
+  ];
 
-    return <>
-    <Link href="/course/new">교육 만들기</Link>
-    <h1>교육 목록</h1>
-    <div style={{ display: "flex", flexDirection: "column" }}>
-        {courses.map(course => (
-            <Link key={course.id} href={`/course/${course.id}`}>{course.title}</Link>
-        ))}
-    </div>
-</>
+  return (
+    <>
+      <ContentNavigation links={courseLinks} />
+      <article className={coursestyle.wrapper}>
+        <h1>교육 목록</h1>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {courses.map((course) => (
+            <Link key={course.id} href={`/course/${course.id}`}>
+              {course.title}
+            </Link>
+          ))}
+        </div>
+      </article>
+    </>
+  );
 }
