d13ffb1100c9f913093210a9c1a310e95a5d095d
junhojava <junho9341@gmail.com>
2023-11-27 08:11:41 +0900

    미션 제출 수정 페이지 폼 스타일 추가

--- components/SubmitEditContent.jsx
+++ components/SubmitEditContent.jsx
@@ -4,29 +4,44 @@ import { useRouter } from "next/navigation";
 import { useRef } from "react";
 import fetchWithRetry from "../functions/api";
 import Editor from "./Editor";
+import missionstyle from "../app/mission/mission.module.css";
 
 export default function SubmitEditContent({ missionId, submitId, content }) {
-    const router = useRouter();
-    const editorRef = useRef(null);
+  const router = useRouter();
+  const editorRef = useRef(null);
 
-    const handleEditSubmit = async () => {
-        const formData = new FormData();
+  const handleEditSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
 
-        formData.append("content", editorRef.current.editorInstance.getMarkdown());
+    formData.append("content", editorRef.current.editorInstance.getMarkdown());
 
-        const editSubmitResponse = await fetchWithRetry(`/mission/${missionId}/submit/${submitId}/edit`, {
-            method: "PUT",
-            body: formData,
-        });
+    const editSubmitResponse = await fetchWithRetry(
+      `/mission/${missionId}/submit/${submitId}/edit`,
+      {
+        method: "PUT",
+        body: formData,
+      }
+    );
 
-        if (editSubmitResponse.ok) {
-            router.push(`/mission/${missionId}/submit`, undefined, { shallow: true });
-            router.refresh();
-        }
+    if (editSubmitResponse.ok) {
+      router.push(`/mission/${missionId}/submit`, undefined, { shallow: true });
+      router.refresh();
     }
+  };
 
-    return <>
-        <button onClick={handleEditSubmit}>수정하기</button>
-        <Editor editorRef={editorRef} content={content} />
-    </>
+  return (
+    <form className={missionstyle["form-wrapper"]} onSubmit={handleEditSubmit}>
+      <div className={missionstyle["form-button-area"]}>
+        <input
+          className={missionstyle["form-button"]}
+          type="submit"
+          value="수정하기"
+        />
+      </div>
+      <div className={`${missionstyle["form-content-area"]} ${missionstyle["mission-submit-content-area"]}`}>
+        <Editor editorRef={editorRef} content={content} height={"100%"} />
+      </div>
+    </form>
+  );
 }
