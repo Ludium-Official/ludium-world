7debaf6dcd2ca3654b0e9b68921b57f72bfdc32f
junhojava <junho9341@gmail.com>
2023-11-27 08:00:24 +0900

    미션 제출 폼 스타일 추가

--- app/mission/[missionId]/submit/new/page.jsx
+++ app/mission/[missionId]/submit/new/page.jsx
@@ -5,36 +5,49 @@ import { useRef } from "react";
 import Editor from "../../../../../components/Editor";
 import fetchWithRetry from "../../../../../functions/api";
 import { revalidatePath } from "next/cache";
+import missionstyle from "../../../mission.module.css";
 
 export default function NewMissionSubmitPage({ params }) {
-    const { missionId } = params;
-    const router = useRouter();
-    const editorRef = useRef(null);
+  const { missionId } = params;
+  const router = useRouter();
+  const editorRef = useRef(null);
 
-    const handleSaveSubmitMission = async (e) => {
-        e.preventDefault();
+  const handleSaveSubmitMission = async (e) => {
+    e.preventDefault();
 
-        const { editorInstance } = editorRef.current;
-        const formData = new FormData();
+    const { editorInstance } = editorRef.current;
+    const formData = new FormData();
 
-        formData.append("content", editorInstance.getMarkdown());
-        const newMissionSubmitResponse = await fetchWithRetry(`/mission/${missionId}`, {
-            method: "POST",
-            body: formData,
-        });
+    formData.append("content", editorInstance.getMarkdown());
+    const newMissionSubmitResponse = await fetchWithRetry(
+      `/mission/${missionId}`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
 
-        if (newMissionSubmitResponse.ok) {
-            router.push(`/mission/${missionId}`);
-            router.refresh();
-        }
+    if (newMissionSubmitResponse.ok) {
+      router.push(`/mission/${missionId}`);
+      router.refresh();
     }
+  };
 
-    return <article>
-        <h1>미션 제출</h1>
-        <form onSubmit={handleSaveSubmitMission}>
-            <input type="submit" value="제출하기" />
-            <Editor editorRef={editorRef}></Editor>
-        </form>
-    </article>
-        ;
+  return (
+    <form
+      className={missionstyle["form-wrapper"]}
+      onSubmit={handleSaveSubmitMission}
+    >
+      <div className={missionstyle["form-button-area"]}>
+        <input
+          className={missionstyle["form-button"]}
+          type="submit"
+          value="제출하기"
+        />
+      </div>
+      <div className={`${missionstyle["form-content-area"]} ${missionstyle["mission-submit-content-area"]}`}>
+        <Editor editorRef={editorRef} height={"100%"} />
+      </div>
+    </form>
+  );
 }
