f1826825b60d91f6eee7e7c88e64f06373fd58f3
junhojava <junho9341@gmail.com>
2023-11-27 11:20:38 +0900

    돌아가기 버튼 추가

--- app/mission/[missionId]/submit/[submitId]/comment/CommentList.jsx
+++ app/mission/[missionId]/submit/[submitId]/comment/CommentList.jsx
@@ -50,22 +50,37 @@ export default function CommentList({ comments, missionId, submitId }) {
     getComments();
   });
 
+  const handleBack = () => {
+    router.push(`/mission/${missionId}/submit`);
+  };
+
   return (
     <>
       <form onSubmit={handleNewSubmitComment}>
-        <div className={`${missionstyle["form-button-area"]} ${missionstyle["form-button-end"]}`}>
+        <div
+          className={`${missionstyle["form-button-area"]}`}
+        >
+          <button
+            className={missionstyle["form-button"]}
+            type="button"
+            onClick={handleBack}
+          >
+            돌아가기
+          </button>
           <input
             className={missionstyle["form-button"]}
             type="submit"
             value="댓글 추가하기"
           />
         </div>
-        <div className={`${missionstyle["form-content-area"]} ${missionstyle["mission-comment"]}`}>
+        <div
+          className={`${missionstyle["form-content-area"]} ${missionstyle["mission-comment"]}`}
+        >
           <Editor editorRef={editorRef} height={"100%"} />
         </div>
       </form>
       <div className={missionstyle["comment-list"]}>
-        <h1>댓글 이력</h1>
+        <h1 className={`${missionstyle["mission-view-title"]} ${missionstyle["mission-view-title-no-background"]}`}>댓글 이력</h1>
         {commentList.map((comment) => (
           <SubmitComment
             key={comment.id}
