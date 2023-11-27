17264c3d2908782dbc4808a73b302ea072065f06
junhojava <junho9341@gmail.com>
2023-11-27 10:18:10 +0900

    미션 제출 댓글 폼 스타일 추가

--- front-end/app/mission/[missionId]/submit/[submitId]/history/page.jsx
+++ front-end/app/mission/[missionId]/submit/[submitId]/history/page.jsx
@@ -1,21 +1,36 @@
-import Link from "next/link";
+import ContentNavigation from "../../../../../../components/ContentNavigation";
 import SubmitHistory from "../../../../../../components/mission/SubmitHistory";
 import fetchWithRetry from "../../../../../../functions/api";
+import missinostyle from "../../../../mission.module.css";
 
 async function getSubmitHistory(missionId, submitId) {
-    const getSubmitHistoryResponse = await fetchWithRetry(`/mission/${missionId}/submit/${submitId}/history`);
+  const getSubmitHistoryResponse = await fetchWithRetry(
+    `/mission/${missionId}/submit/${submitId}/history`
+  );
 
-    if (!getSubmitHistoryResponse.ok) return [];
+  if (!getSubmitHistoryResponse.ok) return [];
 
-    return await getSubmitHistoryResponse.json();
+  return await getSubmitHistoryResponse.json();
 }
 
-export default async function SubmitHistoryPage({ params: { missionId, submitId } }) {
-    const history = await getSubmitHistory(missionId, submitId);
-    return <div>
-        <Link href="../" as={`/mission/${missionId}/submit`}>미션 제출 목록으로 가기</Link>
+export default async function SubmitHistoryPage({
+  params: { missionId, submitId },
+}) {
+  const history = await getSubmitHistory(missionId, submitId);
+  const links = [
+    {
+      href: `/mission/${missionId}/submit`,
+      text: "돌아가기",
+    },
+  ];
+  return (
+    <>
+      <ContentNavigation links={links} />
+      <div className={missinostyle["mission-view-wrapper"]}>
         {history.map((item) => (
-            <SubmitHistory key={item.id} {...item} />
+          <SubmitHistory key={item.id} {...item} />
         ))}
-    </div>
+      </div>
+    </>
+  );
 }
--- front-end/app/mission/[missionId]/submit/page.jsx
+++ front-end/app/mission/[missionId]/submit/page.jsx
@@ -1,3 +1,4 @@
+import ContentNavigation from "../../../../components/ContentNavigation";
 import SubmitContent from "../../../../components/mission/SubmitContent";
 import fetchWithRetry from "../../../../functions/api";
 import submitstyle from "../../mission.module.css";
@@ -14,22 +15,31 @@ async function getSubmitList(missionId) {
 
 export default async function MissionSubmitListPage({ params: { missionId } }) {
   const submitList = await getSubmitList(missionId);
+  const links = [
+    {
+      href: `/mission/${missionId}`,
+      text: "돌아가기",
+    },
+  ];
 
   return (
-    <div className={submitstyle["mission-submit-wrapper"]}>
-      <h1>미션 제출 목록</h1>
-      <article className={submitstyle["mission-submit-list"]}>
-        <p className={submitstyle["mission-submit-item"]}>내용</p>
-        <p className={submitstyle["mission-submit-item"]}>작성자</p>
-        <p className={submitstyle["mission-submit-item"]}>검증 여부</p>
-        <p className={submitstyle["mission-submit-item"]}></p>
-        <p className={submitstyle["mission-submit-item"]}></p>
-        <p className={submitstyle["mission-submit-item"]}></p>
-        <p className={submitstyle["mission-submit-item"]}></p>
-        {submitList.map((submit) => (
-          <SubmitContent key={submit.id} missionId={missionId} {...submit} />
-        ))}
-      </article>
-    </div>
+    <>
+      <ContentNavigation links={links} />
+      <div className={submitstyle["mission-view-wrapper"]}>
+        <h1 className={`${submitstyle["mission-view-title"]} ${submitstyle["mission-view-title-no-background"]}`}>미션 제출 목록</h1>
+        <article className={submitstyle["mission-submit-list"]}>
+          <p className={submitstyle["mission-submit-item"]}>내용</p>
+          <p className={submitstyle["mission-submit-item"]}>작성자</p>
+          <p className={submitstyle["mission-submit-item"]}>검증 여부</p>
+          <p className={submitstyle["mission-submit-item"]}></p>
+          <p className={submitstyle["mission-submit-item"]}></p>
+          <p className={submitstyle["mission-submit-item"]}></p>
+          <p className={submitstyle["mission-submit-item"]}></p>
+          {submitList.map((submit) => (
+            <SubmitContent key={submit.id} missionId={missionId} {...submit} />
+          ))}
+        </article>
+      </div>
+    </>
   );
 }
