9678b13777fc91fee6fc3b397d9035c3a62c283e
IDKNWHORU <junho9341@gmail.com>
2023-11-28 17:23:38 +0900

    네비게이션 로고 추가

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import GoogleButton from "./GoogleButton";
 import navigationstyle from "./Navigation.module.css";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 export default function Navigation({ googleAuthInfo, gglId, links }) {
   const pathName = usePathname();
@@ -29,24 +30,12 @@ export default function Navigation({ googleAuthInfo, gglId, links }) {
   return (
     <nav className={navigationstyle.wrapper}>
       <ul className={navigationstyle.list}>
+        <li className={navigationstyle.logo}>
+          <Image src="/logo_purple.png" alt="ludium" width={156} height={70} priority />
+        </li>
         {links.map(({id ,dir, text}) => <li key={id}>
             <Link className={(pathName.split("/")[1] === dir.split("/")[1])? `${navigationstyle.link} ${navigationstyle.active}`: navigationstyle.link} href={dir}>{text}</Link>
         </li>)}
-        {/* <li>
-          <Link href="/profile">프로필</Link>
-        </li>
-        <li>
-          <Link href="/mission">미션</Link>
-        </li>
-        <li>
-          <Link href="/article">아티클</Link>
-        </li>
-        <li>
-          <Link href="/post">자유게시판</Link>
-        </li>
-        <li>
-          <Link href="/course">교육</Link>
-        </li> */}
       </ul>
     </nav>
   );
