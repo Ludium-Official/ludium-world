e871cd304c6799d8002639a3a78467d30a1733b9
IDKNWHORU <junho9341@gmail.com>
2023-11-28 14:56:10 +0900

    네비게이션 링크 아이디 랜덤 생성하는 기능 추가

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,17 +2,42 @@ import { cookies } from "next/headers";
 import MainWrapper from "../components/MainWrapper";
 import Navigation from "../components/Navigation";
 
-export default function RootLayout({
+async function getNavigationLinks() {
+    return [{
+        id: crypto.randomUUID(),
+        dir: "/profile",
+        text: "프로필"
+    },{
+        id: crypto.randomUUID(),
+        dir: "/mission",
+        text: "미션"
+    }, {
+        id: crypto.randomUUID(),
+        dir: "/article",
+        text: "아티클"
+    }, {
+        id: crypto.randomUUID(),
+        dir: "/post",
+        text: "자유게시판"
+    }, {
+        id: crypto.randomUUID(),
+        dir: "/course",
+        text: "교육"
+    }];
+}
+
+export default async function RootLayout({
     children
 }) {
     const cookieStore = cookies();
+    const links = await getNavigationLinks();
 
     return <html lang="en">
         <head>
         </head>
         <body style={{margin: 0}}>
             <main style={{ display: "flex" }}>
-                <Navigation googleAuthInfo={cookieStore.get("access_token")} gglId={cookieStore.get("ggl_id")} />
+                <Navigation googleAuthInfo={cookieStore.get("access_token")} gglId={cookieStore.get("ggl_id")} links={links} />
                 <MainWrapper>
                     {children}
                 </MainWrapper>
