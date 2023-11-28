3895ad8ead3e363569df0b866db02d476e7ffe46
IDKNWHORU <junho9341@gmail.com>
2023-11-28 16:16:36 +0900

    회원 가입 페이지 스타일 추가

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,46 +1,9 @@
-"use client";
+import SignUp from "./SignUp"
 
-import Editor from "../../components/Editor";
-import { useRef } from "react";
-import fetchWithRetry from "../../functions/api";
-import { useRouter } from "next/navigation";
+export const metadata = {
+    title: "내 정보 입력"
+}
 
-export default function SignUP() {
-    const router = useRouter();
-    const editorRef = useRef(null);
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const formData = new FormData(e.target);
-
-        const { editorInstance } = editorRef.current;
-        formData.append("self_intro", editorInstance.getMarkdown());
-
-        try {
-            const createUserSignResponse = await fetchWithRetry(`/user/sign-up/google`, {
-                method: "POST",
-                body: formData,
-            })
-
-            if (createUserSignResponse.ok) {
-                router.push("/");
-                router.refresh();
-            }
-
-        } catch (error) {
-
-        }
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="nick">닉네임</label>
-            <input type="text" name="nick" id="nick" placeholder="닉네임을 입력하세요" />
-            <label htmlFor="phone_number">핸드폰번호</label>
-            <input type="number" name="phone_number" id="phone_number" placeholder="'-' 없이 숫자만 입력해주세요." />
-            <p>자기소개</p>
-            <Editor editorRef={editorRef} />
-            <button>취소하기</button>
-            <input type="submit" value="저장하기" />
-        </form>
-    )
+export default function SignUpPage() {
+    return <SignUp />
 }  
