bbad9460799d2cc708876f3f9edf2dd441013479
IDKNWHORU <junho9341@gmail.com>
2023-11-28 16:02:26 +0900

    홈 화면 스타일 추가

--- front-end/app/page.jsx
+++ front-end/app/page.jsx
@@ -1,7 +1,11 @@
+import homestyle from "./home.module.css"
+
 export const metadata = {
     title: "Ludium"
 }
 
 export default function Page() {
-    return "메인 페이지 문구";
+    return <article className={homestyle["home-wrapper"]}>
+        <h1>루디움 메인 페이지</h1>
+    </article>
 }
