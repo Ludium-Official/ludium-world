82a5e76df97f5646d26b4ab48b9280a5a91014ca
IDKNWHORU <junho9341@gmail.com>
2023-11-29 16:41:58 +0900

    교육 조회 페이지 스타일 추가

--- app/course/[courseId]/page.jsx
+++ app/course/[courseId]/page.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Viewer from "../../../components/Viewer";
 import fetchWithRetry from "../../../functions/api";
 import coursestyle from "../course.module.css";
+import ContentNavigation from "../../../components/ContentNavigation";
 
 async function getCourse(courseId) {
     const getCourseResponse = await fetchWithRetry(`/course/${courseId}`);
@@ -11,16 +12,26 @@ async function getCourse(courseId) {
     return await getCourseResponse.json();
 }
 
-export default async function CoursePage({ params: {courseId} }) {
+export default async function CoursePage({ params: { courseId } }) {
     const course = await getCourse(courseId);
+    const links = [{
+        href: "/course",
+        text: "돌아가기"
+    }]
 
-    return <article className={coursestyle.wrapper}>
-        <input type="text" defaultValue={course.title} readOnly />
-        <h1>모듈 목록</h1>
-        {course.modules.map(module => <div key={module.id}>
-            <Link href={`/course/${courseId}/module/${module.id}`}>{module.title}</Link>
-        </div>)}
-        <hr />
-        <Viewer content={course.content} />
-    </article>;
+    return <>
+        <ContentNavigation links={links} />
+        <article className={coursestyle.wrapper}>
+            <h1 className={coursestyle.title}>{course.title}</h1>
+            <section className={coursestyle["content-area"]}>
+                <Viewer content={course.content} height="100%" />
+            </section>
+            <h2 className={coursestyle["title-label"]}>모듈 목록</h2>
+            <section className={`${coursestyle["course-list"]} ${coursestyle["module-list"]}`}>
+                {course.modules.map(module => <h2 className={coursestyle["course-list-item"]} key={module.id}>
+                    <Link href={`/course/${courseId}/module/${module.id}`}>{module.title}</Link>
+                </h2>)}
+            </section>
+        </article>
+    </>
 }
--- app/course/new/page.jsx
+++ app/course/new/page.jsx
@@ -21,7 +21,7 @@ export default function NewCoursePage() {
         const modules = Array.from(moduleFields).reduce((acc, moduleField, index) => {
             const value = moduleField.value;
 
-            if(index === 0) {
+            if (index === 0) {
                 return `${value}`;
             }
 
@@ -50,14 +50,14 @@ export default function NewCoursePage() {
     }
 
     return <form className={coursestyle["course-edit-wrapper"]} onSubmit={handleSave}>
-    <input type="text" name="title" id="title" placeholder="제목을 입력해주세요" />
-    <input type="submit" value="저장하기" />
-    <div style={{display: "flex", flexDirection: "column"}}>
-        <button type="button" onClick={handleCreateModule}>모듈 추가하기</button>
-        {modules.map((_, index) =>
-            <input key={`${id}-${index}`} type="text" name={`module-${index}`} id={`module-${index}`} placeholder="모듈 제목을 입력해주세요" />
-        )}
-    </div>
-    <Editor editorRef={editorRef} />
-</form>
+        <input type="submit" value="저장하기" />
+        <input type="text" name="title" id="title" placeholder="제목을 입력해주세요" />
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            <button type="button" onClick={handleCreateModule}>모듈 추가하기</button>
+            {modules.map((_, index) =>
+                <input key={`${id}-${index}`} type="text" name={`module-${index}`} id={`module-${index}`} placeholder="모듈 제목을 입력해주세요" />
+            )}
+        </div>
+        <Editor editorRef={editorRef} />
+    </form>
 }
