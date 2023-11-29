32c630afaba710eed460dd1ee79fc4465e0802fe
IDKNWHORU <junho9341@gmail.com>
2023-11-29 15:06:45 +0900

    프로필 탭 컴포넌트 추가

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,12 +1,133 @@
 import { cookies } from "next/headers";
+import Link from "next/link";
 import ContentNavigation from "../../components/ContentNavigation";
 import Viewer from "../../components/Viewer";
 import fetchWithRetry from "../../functions/api";
 import profilestyle from "./profile.module.css";
-import Link from "next/link";
 
 export const metadata = {
   title: "프로필"
+}
+
+function Articles({ articles }) {
+  return <article className={`${profilestyle["profile-content"]}`}>
+    {articles.map((article) => (
+      <div
+        key={article.id}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          border: "solid 1px",
+          margin: "0 0 10px 0",
+        }}
+      >
+        <p>{article.title}</p>
+        <p>{article.content}</p>
+      </div>
+    ))}
+  </article>
+}
+
+function Missions({ missions }) {
+  return <article className={`${profilestyle["profile-content"]}`}>
+    {missions.map((article) => (
+      <div
+        key={article.id}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          border: "solid 1px",
+          margin: "0 0 10px 0",
+        }}
+      >
+        <p>{article.title}</p>
+        <p>{article.content}</p>
+      </div>
+    ))}
+  </article>
+}
+
+function FreeBoards({ freeBoards }) {
+  return <article className={`${profilestyle["profile-content"]}`}>
+    {freeBoards.map((article) => (
+      <div
+        key={article.id}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          border: "solid 1px",
+          margin: "0 0 10px 0",
+        }}
+      >
+        <p>{article.title}</p>
+        <p>{article.content}</p>
+      </div>
+    ))}
+  </article>
+}
+
+function Submits({ submits }) {
+  return <article className={`${profilestyle["profile-content"]}`}>
+    {submits.map((submit) => (
+      <div
+        key={submit.id}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <span>
+          {submit.content}
+          <p style={{ color: submit.vldStt === true ? "green" : "red" }}>
+            {submit.vldStt === true ? "검증됨" : "검증되지 않음"}
+          </p>
+        </span>
+      </div>
+    ))}
+  </article>
+}
+
+function Comments({ comments }) {
+  return <article className={`${profilestyle["profile-content"]}`}>
+    {comments.map((comment) => (
+      <p key={comment.id}>{comment.content}</p>
+    ))}
+  </article>
+}
+
+function Replies({ replies }) {
+  return <article className={`${profilestyle["profile-content"]}`}>
+    {replies.map((comment) => (
+      <p key={comment.id}>{comment.content}</p>
+    ))}
+  </article>
+}
+
+function Tabs({ active }) {
+  const links = [{
+    href: "article",
+    text: "아티클"
+  }, {
+    href: "mission",
+    text: "미션"
+  }, {
+    href: "free_board",
+    text: "자유 게시글"
+  }, {
+    href: "submit_mission",
+    text: "제출한 미션"
+  }, {
+    href: "comments",
+    text: "댓글",
+  }, {
+    href: "reply_comments",
+    text: "내 미션에 달린 댓글"
+  }];
+
+  return <>
+    {links.map(({href, text}) => 
+      <h2 className={`${profilestyle["profile-title"]} ${profilestyle["profile-title-tab"]} ${active === href ? profilestyle["profile-title-tab-active"]: ""}`}>
+        <Link href={`/profile?active=${href}`}>{text}</Link>
+      </h2>
+    )}
+  </>
 }
 
 export async function getProfile() {
@@ -23,7 +144,7 @@ export async function getProfile() {
   return await getProfileResopnse.json();
 }
 
-export default async function ProfilePage() {
+export default async function ProfilePage({ searchParams: { active } }) {
   const profile = await getProfile();
   const links = [
     {
@@ -47,111 +168,16 @@ export default async function ProfilePage() {
             <Viewer content={profile.user.selfIntro} />
           </div>
         </article>
-        <h2
-          className={`${profilestyle["profile-title"]} ${profilestyle["profile-title-tab"]} ${profilestyle["profile-title-active"]}`}
-        >
-          <Link href="/profile?active=article">내가 쓴 아티클</Link>
-        </h2>
-        <article className={`${profilestyle["profile-content"]}`}>
-          {profile.articles.map((article) => (
-            <div
-              key={article.id}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                border: "solid 1px",
-                margin: "0 0 10px 0",
-              }}
-            >
-              <p>{article.title}</p>
-              <p>{article.content}</p>
-            </div>
-          ))}
-        </article>
-        <h2
-          className={`${profilestyle["profile-title"]} ${profilestyle["profile-title-tab"]}`}
-        >
-          <Link href="/profile?active=mission">미션</Link>
-        </h2>
-        <article className={`${profilestyle["profile-content"]}`}>
-          {profile.missions.map((article) => (
-            <div
-              key={article.id}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                border: "solid 1px",
-                margin: "0 0 10px 0",
-              }}
-            >
-              <p>{article.title}</p>
-              <p>{article.content}</p>
-            </div>
-          ))}
-        </article>
-        <h2
-          className={`${profilestyle["profile-title"]} ${profilestyle["profile-title-tab"]}`}
-        >
-          <Link href="/profile?active=free_board">자유게시글</Link>
-        </h2>
-        <article className={`${profilestyle["profile-content"]}`}>
-          {profile.freeBoards.map((article) => (
-            <div
-              key={article.id}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                border: "solid 1px",
-                margin: "0 0 10px 0",
-              }}
-            >
-              <p>{article.title}</p>
-              <p>{article.content}</p>
-            </div>
-          ))}
-        </article>
-        <h2
-          className={`${profilestyle["profile-title"]} ${profilestyle["profile-title-tab"]}`}
-        >
-          <Link href="/profile?active=submit_mission">제출한 미션</Link>
-        </h2>
-        <article className={`${profilestyle["profile-content"]}`}>
-          {profile.submits.map((submit) => (
-            <div
-              key={submit.id}
-              style={{ display: "flex", flexDirection: "column" }}
-            >
-              <span>
-                {submit.content}
-                <p style={{ color: submit.vldStt === true ? "green" : "red" }}>
-                  {submit.vldStt === true ? "검증됨" : "검증되지 않음"}
-                </p>
-              </span>
-            </div>
-          ))}
-        </article>
-        <h2
-          className={`${profilestyle["profile-title"]} ${profilestyle["profile-title-tab"]}`}
-        >
-          <Link href="/profile?active=comments">댓글</Link>
-        </h2>
-        <article className={`${profilestyle["profile-content"]}`}>
-          {profile.comments.map((comment) => (
-            <p key={comment.id}>{comment.content}</p>
-          ))}
-        </article>
-        <h2
-          className={`${profilestyle["profile-title"]} ${profilestyle["profile-title-tab"]}`}
-        >
-          <Link href="/profile?active=replay_comments">
-            내 미션에 달린 댓글
-          </Link>
-        </h2>
-        <article className={`${profilestyle["profile-content"]}`}>
-          {profile.userCommentsByMyMission.map((comment) => (
-            <p key={comment.id}>{comment.content}</p>
-          ))}
-        </article>
+        <Tabs active={active} />
+        {
+          active === "article" ? <Articles articles={profile.articles} /> :
+            active === "mission" ? <Missions missions={profile.missions} /> :
+              active === "free_board" ? <FreeBoards freeBoards={profile.freeBoards} /> :
+                active === "submit_mission" ? <Submits submits={profile.submits} /> :
+                  active === "comments" ? <Comments comments={profile.comments} /> :
+                    active === "reply_comments" ? <Replies replies={profile.userCommentsByMyMission} /> :
+                      <Articles articles={profile.articles} />
+        }
       </div>
     </>
   );
