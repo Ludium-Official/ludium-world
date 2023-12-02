c5178008f18fdb3e70ea218a0d9f3bd36335078c
IDKNWHORU <junho9341@gmail.com>
2023-12-02 20:35:45 +0900

    미션 삭제 기능 추가

--- src/main/java/world/ludium/education/mission/MissionSubmitCommentRepository.java
+++ src/main/java/world/ludium/education/mission/MissionSubmitCommentRepository.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 public interface MissionSubmitCommentRepository extends JpaRepository<MissionSubmitComment, UUID> {
     List<MissionSubmitComment> findAllByMsnSbmId(UUID msnSbmId);
     List<MissionSubmitComment> findAllByUsrId(UUID usrId);
+
+    void deleteAllByMsnSbmId(UUID msnSbmId);
 }
