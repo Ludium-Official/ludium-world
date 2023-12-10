1816808da82abfdf633f9bee97c02daa520d547d
IDKNWHORU <junho9341@gmail.com>
2023-12-10 17:10:05 +0900

    공고 추가 페이지에서 모듈 만들기 기능 제거

--- admin/app/announcement/new/NewAnnouncement.jsx
+++ admin/app/announcement/new/NewAnnouncement.jsx
@@ -8,33 +8,14 @@ import announcementstyle from "../announcement.module.css";
 
 export default function NewAnnouncement() {
   const editorRef = useRef(null);
-  const [modules, setModules] = useState([]);
   const router = useRouter();
 
   const handleSave = async (e) => {
     e.preventDefault();
 
     const { editorInstance } = editorRef.current;
-    const moduleFields = e.target.querySelectorAll(`[name^="module-"]`);
-
-    const modules = Array.from(moduleFields).reduce(
-      (acc, moduleField, index) => {
-        const value = moduleField.value;
-
-        if (index === 0) {
-          return `${value}`;
-        }
-
-        return `${acc}|${value}`;
-      },
-      ""
-    );
-
-    const formData = new FormData();
-
-    formData.append("title", e.target.title.value);
+    const formData = new FormData(e.target);
     formData.append("content", editorInstance.getMarkdown());
-    formData.append("modules", modules);
 
     const createAnnouncementResponse = await fetchWithRetry(`/announcement`, {
       method: "POST",
@@ -45,10 +26,6 @@ export default function NewAnnouncement() {
       router.push("/announcement");
       router.refresh();
     }
-  };
-
-  const handleCreateModule = () => {
-    setModules([...modules, { title: "" }]);
   };
 
   const handleBack = () => {
@@ -79,29 +56,6 @@ export default function NewAnnouncement() {
           id="title"
           placeholder="제목을 입력해주세요"
         />
-      </div>
-      <div className={`${announcementstyle["form-button-area"]} ${announcementstyle["form-list-add-button-area"]}`}>
-        <button
-          className={announcementstyle["form-button"]}
-          type="button"
-          onClick={handleCreateModule}
-        >
-          모듈 추가하기
-        </button>
-      </div>
-      <div className={announcementstyle["form-list-area"]}>
-        <div className={announcementstyle["form-list-inner"]}>
-          {modules.map((_, index) => (
-            <input
-              className={announcementstyle["form-textfield"]}
-              key={crypto.randomUUID()}
-              type="text"
-              name={`module-${index}`}
-              id={`module-${index}`}
-              placeholder="모듈 제목을 입력해주세요"
-            />
-          ))}
-        </div>
       </div>
       <div className={announcementstyle["form-content-area"]}>
         <Editor editorRef={editorRef} height="100%" />
