7fd46b1e9e49cd6000427fefcc8eb7202d9cbbca
IDKNWHORU <junho9341@gmail.com>
2023-12-10 14:09:05 +0900

    사용자 권한을 설정하는 기능 추가
    회원 가입 할 때 사용자 권한을 설정하는 기능 추가
    제작자 권한을 설정하는 기능 추가
    관리자 권한을 설정하는 기능 추가
    사용자 권한 설정 ddl 추가

--- src/main/java/world/ludium/education/auth/UserController.java
+++ src/main/java/world/ludium/education/auth/UserController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.*;
 import world.ludium.education.auth.google.GoogleUser;
 import world.ludium.education.auth.google.GoogleUserService;
 import world.ludium.education.auth.ludium.LudiumUser;
+import world.ludium.education.auth.ludium.LudiumUserRight;
 import world.ludium.education.auth.ludium.LudiumUserService;
+import world.ludium.education.auth.ludium.UserDTO;
 
 import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user", produces = "application/json")
@@ -37,10 +42,11 @@ public class UserController {
             googleUserApiData = loginService.getUserResource(accessToken, "google");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                    new HashMap<String, String>() {{
-                        put("message", "인증에 실패했습니다.");
-                        put("debug", e.getMessage());
-                    }
+                    new HashMap<String, String>() {
+                        {
+                            put("message", "인증에 실패했습니다.");
+                            put("debug", e.getMessage());
+                        }
                     });
         }
 
@@ -74,5 +80,74 @@ public class UserController {
             put("self_intro", self_intro);
             put("phn_nmb", phone_number);
         }});
+    }
+
+    @GetMapping("")
+    public ResponseEntity getAllUser() {
+        List<UserDTO> userDTOList = ludiumUserService.getAllUser()
+                .stream()
+                .map(user -> {
+                    LudiumUserRight userRight = ludiumUserService.getUserRight(user.getId());
+                    GoogleUser googleUser = googleUserService.getUserById(user.getGglId());
+
+                    UserDTO userDTO = new UserDTO();
+                    userDTO.setId(user.getId());
+                    userDTO.setNick(user.getNick());
+                    userDTO.setEmail(googleUser.getGgl_eml());
+                    userDTO.setContributor(userRight.isCrt());
+                    userDTO.setProvider(userRight.isPrv());
+                    userDTO.setAdmin(userRight.isAdm());
+
+                    return userDTO;
+                })
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(userDTOList);
+    }
+
+    @PutMapping("/{userId}/provider")
+    public ResponseEntity updateProviderRight(@CookieValue(name = "access_token", required = false) String accessToken,
+                                          @PathVariable UUID userId,
+                                          @RequestParam boolean isProvider) {
+        LudiumUserRight provider = ludiumUserService.getUserRight(userId);
+
+        provider.setPrv(isProvider);
+
+        try {
+            ludiumUserService.updateUserRight(provider);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new HashMap<String, String>() {
+                        {
+                            put("message", "프로바이더를 설정하는 중에 에러가 발생하였습니다.");
+                            put("debug", e.getMessage());
+                        }
+                    });
+        }
+
+        return ResponseEntity.ok(provider);
+    }
+
+    @PutMapping("/{userId}/admin")
+    public ResponseEntity updateAdminRight(@CookieValue(name = "access_token", required = false) String accessToken,
+                                          @PathVariable UUID userId,
+                                          @RequestParam boolean isAdmin) {
+        LudiumUserRight admin = ludiumUserService.getUserRight(userId);
+
+        admin.setAdm(isAdmin);
+
+        try {
+            ludiumUserService.updateUserRight(admin);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new HashMap<String, String>() {
+                        {
+                            put("message", "관리자를 설정하는 중에 에러가 발생하였습니다.");
+                            put("debug", e.getMessage());
+                        }
+                    });
+        }
+
+        return ResponseEntity.ok(admin);
     }
 }
