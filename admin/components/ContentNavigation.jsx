e3d4e61cc3d5167773af7fe4a0e71f192b05bc1a
IDKNWHORU <junho9341@gmail.com>
2023-12-10 16:46:27 +0900

    모듈 안에 제작을 생성하는 기능 추가

--- admin/components/ContentNavigation.jsx
+++ admin/components/ContentNavigation.jsx
@@ -7,9 +7,11 @@ export default function ContentNavigation({ links, children }) {
             {links.map(link => <li key={`${crypto.randomUUID()}`}>
                 <Link href={link.href}>{link.text}</Link>
             </li>)}
-            {children ? <li>
-                {children}
-            </li> : null}
+            {children ? Array.isArray(children) ? children.map(child => <li>
+                {child}
+            </li>) : <li>
+            {children}
+        </li> : null}
         </ul>
     </nav>
 }
