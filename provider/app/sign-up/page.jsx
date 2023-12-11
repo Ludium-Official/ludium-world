95f1d54f2e1dde6b2db6166fb1b053e1654e5a50
IDKNWHORU <junho9341@gmail.com>
2023-12-11 14:10:10 +0900

    프로바이더 사이트에서 회원 가입할 때 신청서 내용을 불러오도록 변경

--- provider/app/sign-up/page.jsx
+++ provider/app/sign-up/page.jsx
@@ -1,9 +1,20 @@
+import fetchWithRetry from "../../functions/api"
 import SignUp from "./SignUp"
 
 export const metadata = {
     title: "내 정보 입력"
 }
 
-export default function SignUpPage() {
-    return <SignUp />
+async function getApply() {
+    const applyResponse = await fetchWithRetry("/apply");
+
+    if(!applyResponse.ok) return null;
+
+    return await applyResponse.json();
+}
+
+export default async function SignUpPage() {
+    const apply = await getApply();
+
+    return <SignUp apply={apply} />
 }  
