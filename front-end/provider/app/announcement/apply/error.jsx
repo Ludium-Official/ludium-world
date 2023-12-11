f227b2e9078bb4e9cebaf30db454709e06340db4
IDKNWHORU <junho9341@gmail.com>
2023-12-11 16:54:39 +0900

    인증되지 않은 사용자가 화면 접속할 때 에러페이지로 리디렉션하는 기능 추가

--- front-end/provider/app/announcement/apply/error.jsx
+++ front-end/provider/app/announcement/apply/error.jsx
@@ -1,20 +1,17 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import profilestyle from "./profile.module.css";
+import applystyle from "./apply.module.css";
 
-export default function ProfileErrorPage({
-    error,
-    reset,
-}) {
+export default function ApplyErrorPage({error}) {
     const router = useRouter();
 
     const handleSignUp = () => {
         router.replace("/sign-up");
     }
 
-    return <div className={profilestyle["profile-wrapper"]}>
+    return <article className={`${applystyle.wrapper} ${applystyle.error}`}>
         <h1>로그인 혹은 회원가입을 먼저 해주세요.</h1>
         <button onClick={handleSignUp}>회원가입 하러 가기</button>
-    </div>
+    </article>
 }
