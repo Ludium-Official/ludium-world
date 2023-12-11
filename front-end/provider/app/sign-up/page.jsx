5c9409013a87df53adc55506fef2f9ef789d5c33
IDKNWHORU <junho9341@gmail.com>
2023-12-11 14:35:43 +0900

    프로바이더 페이지에서 지원서 데이터를 불러오는 기능 제거

--- front-end/provider/app/sign-up/page.jsx
+++ front-end/provider/app/sign-up/page.jsx
@@ -1,20 +1,9 @@
-import fetchWithRetry from "../../functions/api"
 import SignUp from "./SignUp"
 
 export const metadata = {
     title: "내 정보 입력"
 }
 
-async function getApply() {
-    const applyResponse = await fetchWithRetry("/apply");
-
-    if(!applyResponse.ok) return null;
-
-    return await applyResponse.json();
-}
-
-export default async function SignUpPage() {
-    const apply = await getApply();
-
+export default function SignUpPage() {
     return <SignUp apply={apply} />
 }  
