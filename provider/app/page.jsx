acfbb79a5c9945bceb383183019b83f3829e9034
IDKNWHORU <junho9341@gmail.com>
2023-12-18 16:50:35 +0900

    프로바이더 페이지의 메인 화면에 공고 목록을 추가

--- provider/app/page.jsx
+++ provider/app/page.jsx
@@ -1,11 +1,9 @@
-import homestyle from "./home.module.css"
+import AnnouncementPage from "./announcement/page"
 
 export const metadata = {
     title: "Ludium"
 }
 
 export default function Page() {
-    return <article className={homestyle["home-wrapper"]}>
-        <h1>루디움 메인 페이지</h1>
-    </article>
+    return <AnnouncementPage />
 }
