95011f666441d74c3f91553c0bfc62f6b71faaf2
junhojava <junho9341@gmail.com>
2023-12-19 22:38:58 +0900

    제작을 생성할 때 Article 테이블에도 제작을 만들도록 변경

--- src/main/java/world/ludium/education/course/ModuleService.java
+++ src/main/java/world/ludium/education/course/ModuleService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import world.ludium.education.apply.ModuleApplyReference;
 import world.ludium.education.apply.ModuleApplyReferenceRepository;
-import world.ludium.education.article.Article;
 
 import java.util.List;
 import java.util.UUID;
