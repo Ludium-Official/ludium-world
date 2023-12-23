aa2dec2cdde0dec2eb92883db2525a5f92af56ac
IDKNWHORU <junho9341@gmail.com>
2023-12-23 15:51:23 +0900

    구글 로그인할 때 refresh 토큰을 발급받기 위해 consent 옵션 추가

--- provider/components/GoogleButton.jsx
+++ provider/components/GoogleButton.jsx
@@ -11,7 +11,7 @@ export default function GoogleButton() {
     const router = useRouter();
 
     const handleGoogleButtonClick = () => {
-        router.push(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&access_type=offline`);
+        router.push(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&access_type=offline&prompt=consent`);
         router.refresh();
     }
 
