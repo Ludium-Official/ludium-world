ab3e9828889fb09a328963e2eddee914c3c27f6d
junhojava <junho9341@gmail.com>
2023-12-31 23:27:55 +0900

    세부 공고 엔티티를 detailed_posting으로 변경

--- admin/app/announcement/[announcementId]/ModuleCreateButton.jsx
+++ admin/app/announcement/[announcementId]/ModuleCreateButton.jsx
@@ -4,19 +4,19 @@ import { useRouter } from "next/navigation";
 import fetchWithRetry from "../../../functions/api";
 
 export default function ModuleCreateButton({ announceId }) {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleCreateMake = async () => {
-        const makeFormData = new FormData();
+  const handleCreateMake = async () => {
+    const makeFormData = new FormData();
 
-        makeFormData.append("title", "모듈 제목을 입력해주세요");
+    makeFormData.append("title", "모듈 제목을 입력해주세요");
 
-        const createModuleWithArticle = await fetchWithRetry(`/announcement/${announceId}`, {
-            method: "POST",
-            body: makeFormData
-        });
+    await fetchWithRetry(`/announcement/${announceId}`, {
+      method: "POST",
+      body: makeFormData,
+    });
 
-        router.refresh();
-    }
-    return <button onClick={handleCreateMake}>모듈 추가하기</button>
+    router.refresh();
+  };
+  return <button onClick={handleCreateMake}>세부 공고 추가하기</button>;
 }
