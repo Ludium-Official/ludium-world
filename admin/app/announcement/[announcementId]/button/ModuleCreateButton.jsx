bcde6faca4e0aaaea632cab7d4cba5a2ff6144fd
junhojava <junho9341@gmail.com>
2024-01-03 22:48:11 +0900

    세부공고, 제작자 키워드 변경
    세부공고를 작업으로 변경
    제작자를 작업자로 변경

--- admin/app/announcement/[announcementId]/button/ModuleCreateButton.jsx
+++ admin/app/announcement/[announcementId]/button/ModuleCreateButton.jsx
@@ -18,14 +18,14 @@ export default function ModuleCreateButton({ announceId }) {
     );
 
     if (!createDetailedAnnouncementResponse.ok)
-      throw new Error("세부 공고를 만드는 중 에러가 발생했습니다.");
+      throw new Error("작업을 만드는 중 에러가 발생했습니다.");
 
     setPending(false);
     router.refresh();
   };
   return (
     <button onClick={handleCreateMake} disabled={pending}>
-      {pending ? "세부 공고 추가하는 중입니다..." : "세부 공고 추가하기"}
+      {pending ? "작업 추가하는 중입니다..." : "작업 추가하기"}
     </button>
   );
 }
