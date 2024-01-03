1ec57befab96e07aa5961ecbd055cd52948515b6
junhojava <junho9341@gmail.com>
2024-01-03 14:43:17 +0900

    프로바이더 페이지에서 공고 데이터를 조회하는 기능 수정

--- front-end/provider/app/announcement/[announcementId]/page.jsx
+++ front-end/provider/app/announcement/[announcementId]/page.jsx
@@ -1,25 +1,80 @@
+import BackButton from "../../../components/BackButton";
 import ContentNavigation from "../../../components/ContentNavigation";
 import Viewer from "../../../components/Viewer";
 import fetchWithRetry from "../../../functions/api";
-import announcementstyle from "../announcement.module.css";
-import { ModuleViewer } from "./module/[moduleId]/page";
-import BackButton from "../../../components/BackButton";
 import ModuleNavigation from "../ModuleNavigation";
-import Link from "next/link";
-import RedirectApply from "./RedirectApply";
+import announcementstyle from "../announcement.module.css";
+// import RedirectApply from "./RedirectApply";
 
-export const metadata = {
-  title: "공고",
-};
+export async function generateMetadata({ params: { announcementId } }) {
+  const announcement = await getAnnouncement(announcementId);
 
+  return {
+    title: announcement.title,
+  };
+}
 export async function getAnnouncement(announcementId) {
   const getAnnouncementResponse = await fetchWithRetry(
     `/announcement/${announcementId}`
   );
 
-  if (!getAnnouncementResponse.ok) return null;
+  if (!getAnnouncementResponse.ok)
+    throw new Error("공고를 불러오는 중 에러가 발생했습니다.");
 
   return await getAnnouncementResponse.json();
+}
+
+async function getDetailedAnnouncementList(announcementId) {
+  const getDetailedAnnouncementListResponse = await fetchWithRetry(
+    `/announcement/${announcementId}/detail`
+  );
+
+  if (!getDetailedAnnouncementListResponse.ok)
+    throw new Error("작업을 불러오는 중 에러가 발생했습니다.");
+
+  return await getDetailedAnnouncementListResponse.json();
+}
+
+async function DetailedAnnouncementList({ announcementId }) {
+  const detailedAnnouncementList = await getDetailedAnnouncementList(
+    announcementId
+  );
+
+  return (
+    <section
+      className={`${announcementstyle["announcement-list"]} ${announcementstyle["module-list"]}`}
+    >
+      {detailedAnnouncementList.map(({ detailId, title, description }) => (
+        <article key={detailId}>
+          <ModuleNavigation
+            links={[
+              {
+                href: `/announcement/${announcementId}/module/${detailId}/apply`,
+                text: "지원서 작성하기",
+              },
+            ]}
+          />
+          <details className={announcementstyle.module} open={true}>
+            <summary>{title === "" ? "작업" : title} 펼치기 / 닫기</summary>
+            <div className={announcementstyle["module-header-area"]}>
+              <input
+                type="text"
+                defaultValue={
+                  title === ""
+                    ? "작업의 내용이 아직 작성되지 않았습니다."
+                    : title
+                }
+                readOnly
+              />
+            </div>
+            <div className={announcementstyle["module-content"]}>
+              <Viewer content={description} />
+            </div>
+          </details>
+        </article>
+      ))}
+    </section>
+  );
 }
 
 export default async function AnnouncementPage({ params: { announcementId } }) {
@@ -31,38 +86,16 @@ export default async function AnnouncementPage({ params: { announcementId } }) {
         <BackButton />
       </ContentNavigation>
       <article className={announcementstyle.wrapper}>
-        <RedirectApply modules={announcement.modules} announcementId={announcementId} />
+        {/* <RedirectApply
+          modules={announcement.modules}
+          announcementId={announcementId}
+        /> */}
         <h1 className={announcementstyle.title}>{announcement.title}</h1>
         <section className={announcementstyle["content-area"]}>
-          <Viewer content={announcement.content} height="100%" />
+          <Viewer content={announcement.description} height="100%" />
         </section>
-        <h2 className={announcementstyle["title-label"]}>모듈 목록</h2>
-        <section
-          className={`${announcementstyle["announcement-list"]} ${announcementstyle["module-list"]}`}
-        >
-          {announcement.modules.map((module) => (
-            <article
-              className={announcementstyle.module}
-              key={crypto.randomUUID()}
-            >
-              <ModuleNavigation
-                links={[{
-                  href: `/announcement/${announcementId}/module/${module.id}/apply`,
-                  text: "지원서 작성하기",
-                },
-                  {
-                    href: `/announcement/${announcementId}/module/${module.id}`,
-                    text: "모듈보기",
-                  },
-                ]}
-              />
-              <ModuleViewer
-                announcementId={announcementId}
-                moduleId={module.id}
-              />
-            </article>
-          ))}
-        </section>
+        <h2 className={announcementstyle["title-label"]}>작업 목록</h2>
+        <DetailedAnnouncementList announcementId={announcementId} />
       </article>
     </>
   );
