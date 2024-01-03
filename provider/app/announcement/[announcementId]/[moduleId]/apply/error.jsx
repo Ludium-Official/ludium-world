900b49b228af3ec7c9606f46c0856187ca02eb10
junhojava <junho9341@gmail.com>
2024-01-03 16:36:07 +0900

    프로바이더 페이지에서 지원서를 작성하는 기능 추가

---
+++ provider/app/announcement/[announcementId]/[moduleId]/apply/error.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import BackButton from "@/components/BackButton";
+import applystyle from "./apply.module.css";
+
+export default function ApplyErrorPage({ error }) {
+  const handleSignUp = () => {
+    router.replace("/sign-up");
+  };
+
+  const getApplicationErrorComponents = (status) => {
+    switch (status) {
+      case "401":
+        return (
+          <>
+            <h1>로그인 혹은 회원가입을 먼저 해주세요.</h1>
+            <button onClick={handleSignUp}>회원가입 하러 가기</button>
+          </>
+        );
+      case "404":
+        return <h1>지원서 양식이 작성되지 않았습니다.</h1>;
+    }
+  };
+
+  return (
+    <article className={`${applystyle.wrapper} ${applystyle.error}`}>
+      <BackButton />
+      {getApplicationErrorComponents(error.message)}
+    </article>
+  );
+}
---
+++ provider/app/announcement/[announcementId]/[moduleId]/apply/page.jsx
@@ -0,0 +1,71 @@
+import fetchWithRetry from "@/functions/api";
+import { cookies } from "next/headers";
+import ApplyForm from "./ApplyForm";
+import RedirectEditPage from "./RedirectEditPage";
+
+export const metadata = {
+  title: "지원서 작성하기",
+};
+
+export async function getApplicationTemplate(announcementId, detailId, role) {
+  const getApplicationTemplateResponse = await fetchWithRetry(
+    `/announcement/${announcementId}/${detailId}/application-template?role=${role}`
+  );
+
+  if (!getApplicationTemplateResponse.ok)
+    if (getApplicationTemplateResponse.status === 404) throw new Error(404);
+
+  return await getApplicationTemplateResponse.json();
+}
+
+export async function getApplication(announcementId, detailId, role) {
+  const cookieStore = cookies();
+
+  if (cookieStore.get("access_token") === undefined) throw new Error(401);
+
+  const submitApplyResponse = await fetchWithRetry(
+    `/announcement/${announcementId}/${detailId}/application/submit?role=${role}`,
+    {
+      headers: {
+        cookie: cookieStore,
+      },
+    }
+  );
+
+  if (!submitApplyResponse.ok)
+    if (submitApplyResponse.status === 403) throw new Error(403);
+    else if (submitApplyResponse.status === 404) return null;
+    else return null;
+
+  return await submitApplyResponse.json();
+}
+
+export default async function ApplyPage({
+  params: { announcementId, moduleId },
+  searchParams: { role },
+}) {
+  const applicationTemplate = await getApplicationTemplate(
+    announcementId,
+    moduleId,
+    role
+  );
+
+  const submit = await getApplication(announcementId, moduleId, role);
+
+  if (submit)
+    return (
+      <RedirectEditPage
+        announcementId={announcementId}
+        moduleId={moduleId}
+        role={role}
+      />
+    );
+
+  return (
+    <ApplyForm
+      announcementId={announcementId}
+      detailId={moduleId}
+      applicationTemplate={applicationTemplate}
+    />
+  );
+}
