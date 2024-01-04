d136d32b531140b09292685445589d877d35ff22
junhojava <junho9341@gmail.com>
2024-01-04 13:26:02 +0900

    작업 내용을 조회하는 기능 추가

--- src/main/java/world/ludium/education/announcement/DetailedAnnouncementController.java
+++ src/main/java/world/ludium/education/announcement/DetailedAnnouncementController.java
@@ -2,12 +2,13 @@ package world.ludium.education.announcement;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import world.ludium.education.announcement.model.DetailedAnnouncement;
 import world.ludium.education.util.ResponseUtil;
 
-import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/detailed-announcement", produces = "application/json")
@@ -34,6 +35,17 @@ public class DetailedAnnouncementController {
             return responseUtil.getNoSuchElementExceptionMessage("작업 목록 데이터가 없습니다.", nse.getMessage());
         } catch (Exception e) {
             return responseUtil.getExceptionMessage("작업 목록을 조회하는 중에 에러가 발생했습니다.", e.getMessage());
+        }
+    }
+
+    @GetMapping("{detailedAnnouncementId}")
+    public ResponseEntity<Object> getDetailedAnnouncement(@PathVariable UUID detailedAnnouncementId){
+        try {
+            return ResponseEntity.ok(detailedAnnouncementService.getDetailedAnnouncement(detailedAnnouncementId));
+        } catch (NoSuchElementException nse) {
+            return responseUtil.getNoSuchElementExceptionMessage("작업 데이터가 없습니다.", nse.getMessage());
+        } catch (Exception e) {
+            return responseUtil.getExceptionMessage("작업을 조회하는 중에 에러가 발생했습니다.", e.getMessage());
         }
     }
 }
