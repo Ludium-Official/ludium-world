8c1e6d9d02bd1673f2a3a94e189cde6305cb9938
junhojava <junho9341@gmail.com>
2024-01-05 18:03:04 +0900

    작업자 데이터 불일치 예외처리 추가
    작업자가 없을 때(404)
    작업자가 일치하지 않을때(403)

--- front-end/provider/components/Editor.jsx
+++ front-end/provider/components/Editor.jsx
@@ -2,27 +2,32 @@ import { useEffect } from "react";
 import "@toast-ui/editor/dist/toastui-editor.css";
 
 export default function Editor({ editorRef, content, height }) {
-    useEffect(() => {
-        const putEditor = async () => {
-            const toastEditor = await import("@toast-ui/editor");
+  useEffect(() => {
+    const putEditor = async () => {
+      const toastEditor = await import("@toast-ui/editor");
 
-            try {
-                editorRef.current.innerText = "";
+      try {
+        editorRef.current.innerText = "";
 
-                editorRef.current.editorInstance = new toastEditor.Editor({
-                    el: editorRef.current,
-                    height: height ?? "300px",
-                    initialEditType: "wysiwyg",
-                    previewStyle: "tab",
-                    initialValue: content
-                });
-            } catch (error) {
-                console.error(error);
-            }
-        }
+        editorRef.current.editorInstance = new toastEditor.Editor({
+          el: editorRef.current,
+          height: height ?? "300px",
+          initialEditType: "wysiwyg",
+          previewStyle: "tab",
+          initialValue: content,
+          autofocus: false,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        putEditor();
-    }, []);
+    putEditor();
+  }, []);
 
-    return <div ref={editorRef} style={{ width: "100%", height: "100vh" }}>에디터를 불러오는 중입니다...</div>;
+  return (
+    <div ref={editorRef} style={{ width: "100%", height: "100vh" }}>
+      에디터를 불러오는 중입니다...
+    </div>
+  );
 }
