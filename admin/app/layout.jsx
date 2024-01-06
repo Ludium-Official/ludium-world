6f9c64e8a530855b43a65c7528e5d143eb3604c6
junhojava <junho9341@gmail.com>
2024-01-06 23:05:55 +0900

    관리자페이지에서 작업물 댓글을 다는 기능 추가

--- admin/app/layout.jsx
+++ admin/app/layout.jsx
@@ -3,31 +3,32 @@ import MainWrapper from "../components/MainWrapper";
 import Navigation from "../components/Navigation";
 
 async function getNavigationLinks() {
-    return [{
-        id: crypto.randomUUID(),
-        dir: "/profile?active=article",
-        text: "프로필"
-    }, {
-        id: crypto.randomUUID(),
-        dir: "/announcement",
-        text: "공고"
-    }, {
-        id: crypto.randomUUID(),
-        dir: "/make",
-        text: "제작"
-    }, {
-        id: crypto.randomUUID(),
-        dir: "/validate",
-        text: "검증"
-    }, {
-        id: crypto.randomUUID(),
-        dir: "/management",
-        text: "사용자 관리"
-    }, {
-        id: crypto.randomUUID(),
-        dir: "/apply",
-        text: "지원서 관리"
-    }, 
+  return [
+    {
+      id: crypto.randomUUID(),
+      dir: "/profile?active=article",
+      text: "프로필",
+    },
+    {
+      id: crypto.randomUUID(),
+      dir: "/announcement",
+      text: "공고",
+    },
+    {
+      id: crypto.randomUUID(),
+      dir: "/work",
+      text: "작업",
+    },
+    {
+      id: crypto.randomUUID(),
+      dir: "/management",
+      text: "사용자 관리",
+    },
+    {
+      id: crypto.randomUUID(),
+      dir: "/apply",
+      text: "지원서 관리",
+    },
     // , {
     //     id: crypto.randomUUID(),
     //     dir: "/mission",
@@ -45,25 +46,26 @@ async function getNavigationLinks() {
     //     dir: "/course",
     //     text: "교육"
     // }
-];
+  ];
 }
 
-export default async function RootLayout({
-    children
-}) {
-    const cookieStore = cookies();
-    const links = await getNavigationLinks();
+export default async function RootLayout({ children }) {
+  const cookieStore = cookies();
+  const links = await getNavigationLinks();
 
-    return <html lang="en">
-        <head>
-        </head>
-        <body style={{margin: 0}}>
-            <main style={{ display: "flex" }}>
-                <Navigation googleAuthInfo={cookieStore.get("access_token")} gglId={cookieStore.get("ggl_id")} links={links} />
-                <MainWrapper>
-                    {children}
-                </MainWrapper>
-            </main>
-        </body>
+  return (
+    <html lang="en">
+      <head></head>
+      <body style={{ margin: 0 }}>
+        <main style={{ display: "flex" }}>
+          <Navigation
+            googleAuthInfo={cookieStore.get("access_token")}
+            gglId={cookieStore.get("ggl_id")}
+            links={links}
+          />
+          <MainWrapper>{children}</MainWrapper>
+        </main>
+      </body>
     </html>
+  );
 }
