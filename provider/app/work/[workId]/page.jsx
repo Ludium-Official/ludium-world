e8861775cac8e6be3ace261242e24523299903dd
junhojava <junho9341@gmail.com>
2024-01-07 11:59:57 +0900

    프로바이더 페이지에서 작업자가 누군지 조회하는 기능 추가

--- provider/app/work/[workId]/page.jsx
+++ provider/app/work/[workId]/page.jsx
@@ -34,6 +34,26 @@ async function getWorkContentList(workId) {
   return await getWorkContentListResponse.json();
 }
 
+async function getWorker(workId) {
+  const getWorkerResponse = await fetchWithRetry(
+    `/detailed-announcement/${workId}/worker`
+  );
+
+  if (!getWorkerResponse.ok)
+    if (getWorkerResponse.status === 404) return null;
+    else throw new Error(500);
+
+  return await getWorkerResponse.json();
+}
+
+async function getUser(usrId) {
+  const getUserResponse = await fetchWithRetry(`/user/${usrId}`);
+
+  if (!getUserResponse.ok) throw new Error(500);
+
+  return await getUserResponse.json();
+}
+
 async function WorkContentList({ workId }) {
   const workContentList = await getWorkContentList(workId);
 
@@ -49,6 +69,16 @@ async function WorkContentList({ workId }) {
   );
 }
 
+async function Worker({ workId }) {
+  const worker = await getWorker(workId);
+
+  if (worker === null) return <p>작업자 없음</p>;
+
+  const user = await getUser(worker.usrId);
+
+  return <p>작업자 : {user.nick}</p>;
+}
+
 export default async function WorkPage({ params: { workId } }) {
   const work = await getWork(workId);
 
@@ -58,6 +88,7 @@ export default async function WorkPage({ params: { workId } }) {
         <BackButton />
       </ContentNavigation>
       <div className={workstyle["work-view-wrapper"]}>
+        <Worker workId={workId} />
         <h1 className={workstyle["work-view-title"]}>{work.title}</h1>
         <div className={workstyle.content}>
           <Viewer content={work.description} height={"100%"} />
