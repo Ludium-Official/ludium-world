9e1c69514d038a98716bd47be11ec4d30f9da677
junhojava <junho9341@gmail.com>
2024-01-08 12:14:50 +0900

    관리자 페이지에 작업 승인 기능 추가

--- admin/app/work/[workId]/page.jsx
+++ admin/app/work/[workId]/page.jsx
@@ -4,6 +4,7 @@ import fetchWithRetry from "@/functions/api";
 import dynamic from "next/dynamic";
 import workstyle from "../work.module.css";
 import WorkContentCommentEditor from "./WorkContentCommentEditor";
+import ApproveWorkButton from "./ApproveWorkButton";
 
 const Viewer = dynamic(() => import("@/components/Viewer"), {
   ssr: false,
@@ -45,6 +46,18 @@ async function getSubmittedWorkContentCommentList(workId, workContentId) {
   return await getSubmittedWorkContentCommentListResponse.json();
 }
 
+async function getWorker(workId) {
+  const getWorkerResponse = await fetchWithRetry(
+    `/detailed-announcement/${workId}/worker`
+  );
+
+  if (!getWorkerResponse.ok)
+    if (getWorkerResponse.status === 404) return null;
+    else throw new Error(500);
+
+  return await getWorkerResponse.json();
+}
+
 async function getUser(usrId) {
   const getUserResponse = await fetchWithRetry(`/user/${usrId}`);
 
@@ -53,6 +66,16 @@ async function getUser(usrId) {
     else throw new Error(500);
 
   return await getUserResponse.json();
+}
+
+async function Worker({ workId }) {
+  const worker = await getWorker(workId);
+
+  if (worker === null) return <p>작업자: 없음</p>;
+
+  const user = await getUser(worker.usrId);
+
+  return <p>작업자: {user.nick}</p>;
 }
 
 async function User({ usrId }) {
@@ -151,8 +174,13 @@ export default async function WorkPage({ params: { workId } }) {
     <>
       <ContentNavigation links={[]}>
         <BackButton />
+        <ApproveWorkButton work={work} />
       </ContentNavigation>
       <div className={workstyle["work-view-wrapper"]}>
+        <div className={workstyle["work-content-comment-header"]}>
+          <Worker workId={workId} />
+          <p>작업 승인 상태: {work.status === "CREATE" ? "미승인" : "승인"}</p>
+        </div>
         <h1 className={workstyle["work-view-title"]}>{work.title}</h1>
         <div className={workstyle.content}>
           <Viewer content={work.description} height={"100%"} />
---
+++ admin/enums/WORK_STATUS.js
@@ -0,0 +1,4 @@
+export default {
+  CREATE: "CREATE",
+  APPROVE: "APPROVE",
+};
