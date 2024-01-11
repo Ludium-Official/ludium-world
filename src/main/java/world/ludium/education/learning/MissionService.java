cb0416a81fcf705a3ba4d25d96f53a9b89975eb7
junhojava <junho9341@gmail.com>
2024-01-12 00:42:04 +0900

    프로바이더 페이지 미션 제출 기능 추가

--- src/main/java/world/ludium/education/learning/MissionService.java
+++ src/main/java/world/ludium/education/learning/MissionService.java
@@ -1,8 +1,7 @@
 package world.ludium.education.learning;
 
 import org.springframework.stereotype.Service;
-import world.ludium.education.learning.model.Mission;
-import world.ludium.education.learning.model.MissionRepository;
+import world.ludium.education.learning.model.*;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -11,13 +10,20 @@ import java.util.UUID;
 @Service
 public class MissionService {
     private final MissionRepository missionRepository;
+    private final EnhancedMissionSubmitRepository enhancedMissionSubmitRepository;
 
-    public MissionService(MissionRepository missionRepository) {
+    public MissionService(MissionRepository missionRepository,
+                          EnhancedMissionSubmitRepository enhancedMissionSubmitRepository) {
         this.missionRepository = missionRepository;
+        this.enhancedMissionSubmitRepository = enhancedMissionSubmitRepository;
     }
 
     public List<Mission> getAllMission(UUID curriculumId) {
         return missionRepository.findAllByCurriculumIdOrderByCreateAt(curriculumId);
+    }
+
+    public Mission getMission(UUID missionId) {
+        return missionRepository.findById(missionId).orElseThrow();
     }
 
     public Mission createMission(Mission mission) {
@@ -29,5 +35,20 @@ public class MissionService {
 
     public Mission updateMission(Mission mission) {
         return missionRepository.save(mission);
+    }
+
+    public EnhancedMissionSubmit getMissionSubmit(UUID missionId, UUID usrId) {
+        return enhancedMissionSubmitRepository.findByMissionIdAndUsrId(missionId, usrId).orElseThrow();
+    }
+
+    public EnhancedMissionSubmit createMissionSubmit(EnhancedMissionSubmit enhancedMissionSubmit) {
+        enhancedMissionSubmit.setCreateAt(new Timestamp(System.currentTimeMillis()));
+        enhancedMissionSubmit.setStatus(EnhancedMissionSubmitStatus.SUBMIT.toString());
+
+        return enhancedMissionSubmitRepository.save(enhancedMissionSubmit);
+    }
+
+    public EnhancedMissionSubmit updateMissionSubmit(EnhancedMissionSubmit enhancedMissionSubmit) {
+        return enhancedMissionSubmitRepository.save(enhancedMissionSubmit);
     }
 }
