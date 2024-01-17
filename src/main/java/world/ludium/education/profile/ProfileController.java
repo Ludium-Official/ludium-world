0526b2f01250dd32e3f63f2df7597323f93143f7
junhojava <junho9341@gmail.com>
2024-01-17 22:01:41 +0900

    프로바이더 페이지에 내 작업 목록을 조회하는 기능 추가

--- src/main/java/world/ludium/education/profile/ProfileController.java
+++ src/main/java/world/ludium/education/profile/ProfileController.java
@@ -3,14 +3,14 @@ package world.ludium.education.profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import world.ludium.education.announcement.ApplicationService;
-import world.ludium.education.article.ArticleService;
-import world.ludium.education.auth.LoginService;
+import world.ludium.education.announcement.DetailedAnnouncementService;
 import world.ludium.education.auth.ludium.LudiumUser;
 import world.ludium.education.auth.ludium.LudiumUserService;
-import world.ludium.education.mission.MissionSubmitService;
 import world.ludium.education.util.ResponseUtil;
 
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(value = "/profile", produces = "application/json")
@@ -18,13 +18,17 @@ public class ProfileController {
     private final LudiumUserService ludiumUserService;
     private final ResponseUtil responseUtil;
     private final ApplicationService applicationService;
+    private final DetailedAnnouncementService detailedAnnouncementService;
+    private static final Logger logger = Logger.getLogger(ProfileController.class.getName());
 
     public ProfileController(LudiumUserService ludiumUserService,
                              ResponseUtil responseUtil,
-                             ApplicationService applicationService) {
+                             ApplicationService applicationService,
+                             DetailedAnnouncementService detailedAnnouncementService) {
         this.ludiumUserService = ludiumUserService;
         this.responseUtil = responseUtil;
         this.applicationService = applicationService;
+        this.detailedAnnouncementService = detailedAnnouncementService;
     }
 
     @GetMapping("")
@@ -50,6 +54,19 @@ public class ProfileController {
         }
     }
 
+    @GetMapping("{usrId}/detailed-announcement")
+    public ResponseEntity<Object> getAllDetailedAnnouncement(@PathVariable UUID usrId) {
+        try {
+            var detailedAnnouncementList = detailedAnnouncementService.getAllDetailedAnnouncementByWorker(usrId);
+
+            if(detailedAnnouncementList.isEmpty()) return responseUtil.getNoSuchElementExceptionMessage("작업 데이터가 없습니다.", "");
+
+            return ResponseEntity.ok(detailedAnnouncementList);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "작업을 조회하는 중에 에러가 발생했습니다", e);
+            return responseUtil.getExceptionMessage("작업을 조회하는 중에 에러가 발생했습니다.", e.getMessage());
+        }
+    }
 
     @PutMapping("")
     public ResponseEntity<Object> updateProfile(@CookieValue(name = "access_token", required = false) String accessToken,
