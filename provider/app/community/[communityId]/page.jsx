db6fb129c1368fd1ee9af2ab90da0f848113a8c3
junhojava <junho9341@gmail.com>
2024-01-19 20:29:34 +0900

    콘텐츠 댓글 목록을 조회하는 기능 추가

--- provider/app/community/[communityId]/page.jsx
+++ provider/app/community/[communityId]/page.jsx
@@ -7,13 +7,51 @@ import dynamic from "next/dynamic";
 import ContentCommentEditor from "./ContentCommentEditor";
 const Viewer = dynamic(() => import("@/components/Viewer"), { ssr: false });
 
-async function getContent(communityId) {
-  const getContentResponse = await fetchWithRetry(`/content/${communityId}`);
+async function getContent(contentId) {
+  const getContentResponse = await fetchWithRetry(`/content/${contentId}`);
 
   if (!getContentResponse.ok)
     throw new Error("콘텐츠를 조회하는 중 에러가 발생했습니다.");
 
   return await getContentResponse.json();
+}
+
+async function getContentCoomentList(contentId) {
+  const getContentCommentListResponse = await fetchWithRetry(
+    `/content/${contentId}/comment`
+  );
+
+  if (!getContentCommentListResponse.ok)
+    if (getContentCommentListResponse.status === 404) return [];
+    else throw new Error("콘텐츠 댓글을 조회하는 중 에러가 발생했습니다.");
+
+  return await getContentCommentListResponse.json();
+}
+
+async function ContentCoomentList({ contentId }) {
+  const contentCommentList = await getContentCoomentList(contentId);
+
+  return (
+    <article className="comment-list">
+      <h2 className="header2">댓글 목록</h2>
+      {contentCommentList.map((contentComment) => (
+        <section
+          className="comment"
+          key={`${contentComment.missionId} ${contentComment.usrId} ${contentComment.createAt}`}
+        >
+          <section className="space-between margin1">
+            <p className="text1">
+              작성자: <UserNick usrId={contentComment.usrId} />
+            </p>
+            <p className="text1">{getTimeStamp(contentComment.createAt)}</p>
+          </section>
+          <div className="comment-content">
+            <Viewer content={contentComment.description} height="100%" />
+          </div>
+        </section>
+      ))}
+    </article>
+  );
 }
 
 export default async function ContentPage({ params: { communityId } }) {
@@ -37,6 +75,7 @@ export default async function ContentPage({ params: { communityId } }) {
         <div className="viewer-content">
           <Viewer content={content.description} height="100%" />
         </div>
+        <ContentCoomentList contentId={communityId} />
         <ContentCommentEditor contentId={communityId} />
       </div>
     </>
