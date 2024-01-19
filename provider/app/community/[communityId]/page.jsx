dc2af7bbbb24d58f3cfca288d9dfa99953eeda1e
junhojava <junho9341@gmail.com>
2024-01-19 19:17:39 +0900

    콘텐츠 조회 페이지 추가

---
+++ provider/app/community/[communityId]/page.jsx
@@ -0,0 +1,42 @@
+import BackButton from "@/components/BackButton";
+import ContentNavigation from "@/components/ContentNavigation";
+import UserNick from "@/components/UserNick";
+import fetchWithRetry from "@/functions/api";
+import { getTimeStamp } from "@/functions/helper";
+import dynamic from "next/dynamic";
+const Viewer = dynamic(() => import("@/components/Viewer"), { ssr: false });
+
+async function getContent(communityId) {
+  const getContentResponse = await fetchWithRetry(`/content/${communityId}`);
+
+  if (!getContentResponse.ok)
+    throw new Error("콘텐츠를 조회하는 중 에러가 발생했습니다.");
+
+  return await getContentResponse.json();
+}
+
+export default async function ContentPage({ params: { communityId } }) {
+  const content = await getContent(communityId);
+
+  return (
+    <>
+      <ContentNavigation links={[]}>
+        <BackButton />
+      </ContentNavigation>
+      <div className="viewer">
+        <div className="space-between">
+          <p className="text1">
+            작성자: <UserNick usrId={content.usrId} />
+          </p>
+          <p className="text1">{getTimeStamp(content.createAt)}</p>
+        </div>
+        <h1 type="text" className="viewer-title">
+          {content.title}
+        </h1>
+        <div className="viewer-content">
+          <Viewer content={content.description} height="100%" />
+        </div>
+      </div>
+    </>
+  );
+}
