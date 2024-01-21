1c3960aba3fcbeaad4e2a232e5463c06c016a022
junhojava <junho9341@gmail.com>
2024-01-22 01:22:48 +0900

    프로바이더 페이지에 최신 공지 컴포넌트 추가

--- provider/app/page.jsx
+++ provider/app/page.jsx
@@ -1,12 +1,16 @@
+import LatestAnnouncement from "@/components/LatestAnnouncement";
 import AnnouncementPage from "./announcement/page";
 
 export const metadata = {
-  title: "Ludium",
+  title: "루디움",
+  description:
+    "이곳에서는 루디움, 루디움의 공지사항 그리고 전반적인 Web3.0에 대한 정보들을 찾아 볼 수 있어.",
 };
 
 export default function Page() {
   return (
     <>
+      <LatestAnnouncement />
       <AnnouncementPage />
     </>
   );
