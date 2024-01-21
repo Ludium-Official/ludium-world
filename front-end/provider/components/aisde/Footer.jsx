461e9f634fa65313aa037b865588350ccd978e57
junhojava <junho9341@gmail.com>
2024-01-21 23:11:39 +0900

    사이드 메뉴 스타일 추가

---
+++ front-end/provider/components/aisde/Footer.jsx
@@ -0,0 +1,22 @@
+import Logo2 from "./Logo2";
+
+export default function Footer() {
+  return (
+    <>
+      <Logo2 />
+      <address className="address">
+        개인정보처리방침 l 이용약관
+        <br />
+        (주)루디움 l 대표자 : 임동선
+        <br />
+        개인정보보호책임자 : 임동선
+        <br />
+        contact : ludium@ludium.com
+        <br />
+        &copy;2024 LUDIUM
+        <br />
+        .ALL RIGHTS RESERVED.
+      </address>
+    </>
+  );
+}
