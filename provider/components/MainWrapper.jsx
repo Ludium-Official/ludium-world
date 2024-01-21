82a2824c244323d1b4ae68f4d529f6aaa81514f1
junhojava <junho9341@gmail.com>
2024-01-22 01:22:48 +0900

    프로바이더 페이지에 최신 공지 컴포넌트 추가

--- provider/components/MainWrapper.jsx
+++ provider/components/MainWrapper.jsx
@@ -1,7 +1,5 @@
 import mainstyle from "./MainWrapper.module.css";
 
 export default function MainWrapper({ children }) {
-  return <div className={mainstyle.wrapper}>
-    {children}
-  </div>;
+  return <div className={mainstyle.wrapper}>{children}</div>;
 }
