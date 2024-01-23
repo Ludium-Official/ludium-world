2720fc6076ece47207abd0efb4bd08ddc989317b
junhojava <junho9341@gmail.com>
2024-01-23 23:42:40 +0900

    프로바이더 페이지 metadata description 수정

--- provider/app/page.jsx
+++ provider/app/page.jsx
@@ -7,7 +7,7 @@ import { Fragment } from "react";
 export const metadata = {
   title: "루디움",
   description:
-    "이곳에서는 루디움, 루디움의 공지사항 그리고 전반적인 Web3.0에 대한 정보들을 찾아 볼 수 있어.",
+    "공지사항 그리고 전반적인 Web3.0에 대한 정보들을 찾아 볼 수 있어.",
 };
 
 async function getTop5Announcement() {
