5910cbf600ecf7b4c95d2caabdf82cd2f9fd7bc4
junhojava <junho9341@gmail.com>
2024-01-24 22:52:34 +0900

    4MB 이상의 이미지를 업로드를 막는 기능 추가

--- provider/next.config.js
+++ provider/next.config.js
@@ -2,12 +2,6 @@ const config = {
   experimental: {
     serverActions: true,
   },
-  api: {
-    bodyParse: {
-      sizeLimit: "30mb",
-      responseLimit: "30mb",
-    },
-  },
 };
 
 module.exports = config;
