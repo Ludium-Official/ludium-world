0077b20247a62373380ac8a12df162ce4b99c1f7
junhojava <junho9341@gmail.com>
2024-01-24 20:32:10 +0900

    관리자 페이지에 에디터 이미지 업로드 기능 추가

--- admin/components/Editor.jsx
+++ admin/components/Editor.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor as ToastEditor } from "@toast-ui/editor";
+import { uploadImage } from "@/functions/actions/ImageUpload";
 
 export default function Editor({ editorRef, content, height }) {
   useEffect(() => {
@@ -15,6 +16,22 @@ export default function Editor({ editorRef, content, height }) {
           previewStyle: "tab",
           initialValue: content,
           autofocus: false,
+          hooks: {
+            async addImageBlobHook(blob, callback) {
+              const formData = new FormData();
+
+              formData.append("image", blob);
+
+              const imageUploadResponse = await uploadImage(formData);
+
+              if (imageUploadResponse === false) {
+                alert("이미지를 업로드 하는 중 에러가 발생했습니다.");
+                return;
+              }
+
+              callback(imageUploadResponse, blob.name);
+            },
+          },
         });
       } catch (error) {
         console.error(error);
