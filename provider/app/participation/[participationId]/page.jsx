f1769e7c85477579ac4225885511b9951477240c
junhojava <junho9341@gmail.com>
2024-01-28 00:34:41 +0900

    학습 참여 페이지 스타일 추가

--- provider/app/participation/[participationId]/page.jsx
+++ provider/app/participation/[participationId]/page.jsx
@@ -1,16 +1,11 @@
 import BackButton from "@/components/BackButton";
-import ContentNavigation from "@/components/ContentNavigation";
 import fetchWithRetry from "@/functions/api";
+import UnAuthorizedError from "errors/UnAuthorizedError";
+import ko_kr from "langs/ko_kr";
 import dynamic from "next/dynamic";
-import MissionSubmitEditor from "./MissionSubmitEditor";
 import { cookies } from "next/headers";
-import MISSIONSUBMIT_STATUS from "@/enums/MISSIONSUBMIT_STATUS";
-import ArticleSubmitbutton from "./ArticleSubmitButton";
-import ARTICLESUBMIT_STATUS from "@/enums/ARTICLESUBMIT_STATUS";
-import MissionSubmitCommentEditor from "./MissionSubmitCommentEditor";
-import { getTimeStamp } from "@/functions/helper";
-import { Fragment } from "react";
 import Link from "next/link";
+import { Fragment } from "react";
 
 const Viewer = dynamic(() => import("@/components/Viewer"), { ssr: false });
 
@@ -65,224 +60,48 @@ async function getMissinoSubmit(learningId, curriculumId, missionId) {
   return await getMissionSubmitResponse.json();
 }
 
-async function getMission(learningId, curriculumId, missionId) {
-  const getMissionResponse = await fetchWithRetry(
-    `/learning/${learningId}/${curriculumId}/mission/${missionId}`
-  );
-
-  if (!getMissionResponse.ok)
-    throw new Error("미션을 조회하는 중 에러가 발생했습니다.");
-
-  return await getMissionResponse.json();
-}
-
 async function getArticleSubmit(learningId, curriculumId, articleId) {
   const cookieStore = cookies();
-  const getArticleSubmitResponse = await fetchWithRetry(
-    `/learning/${learningId}/${curriculumId}/article/${articleId}/submit/user`,
-    {
-      headers: {
-        cookie: cookieStore,
-      },
-    }
-  );
 
-  if (!getArticleSubmitResponse.ok)
-    if (getArticleSubmitResponse.status === 404) return null;
-    else throw new Error("아티클 제출을 조회하는 중 에러가 발생했습니다.");
-
-  return await getArticleSubmitResponse.json();
-}
-
-async function getMissionSubmitCommentList(missionId, usrId) {
-  const getMissionSubmitCommentListResponse = await fetchWithRetry(
-    `/mission/${missionId}/submit/${usrId}/comment`
-  );
-
-  if (!getMissionSubmitCommentListResponse.ok)
-    if (getMissionSubmitCommentListResponse.status === 404) return [];
-    else throw new Error("미션 제출 댓글을 조회하는 중 에러가 발생했습니다.");
-
-  return await getMissionSubmitCommentListResponse.json();
-}
-
-async function getUser(usrId) {
-  const getUserResponse = await fetchWithRetry(`/user/${usrId}`);
-
-  if (!getUserResponse.ok) throw new Error(500);
-
-  return await getUserResponse.json();
-}
-
-async function User({ usrId }) {
-  const user = await getUser(usrId);
-
-  return <p>작성자: {user.nick}</p>;
-}
-
-async function MissionSubmitComment({ missionId, usrId }) {
-  const missionSubmitCommentList = await getMissionSubmitCommentList(
-    missionId,
-    usrId
-  );
-
-  return (
-    <details className="mission-submit-comment">
-      <summary className="mission-submit-comment-summary" />
-      <h4 className="header4">댓글 목록</h4>
-      {missionSubmitCommentList.map((missionSubmitComment) => (
-        <section
-          className="comment"
-          key={`${missionSubmitComment.missionId} ${missionSubmitComment.createAt}`}
-        >
-          <span className="space-between comment-header">
-            <User usrId={missionSubmitComment.commentor} />
-            <p>{getTimeStamp(missionSubmitComment.createAt)}</p>
-          </span>
-          <div className="comment-content">
-            <Viewer content={missionSubmitComment.description} height="100%" />
-          </div>
-        </section>
-      ))}
-      <section>
-        <h4 className="header4">댓글 작성</h4>
-        <MissionSubmitCommentEditor missionId={missionId} usrId={usrId} />
-      </section>
-    </details>
-  );
-}
-
-async function MissionSubmit({ learningId, curriculumId, mission }) {
-  const missionSubmit = await getMissinoSubmit(
-    learningId,
-    curriculumId,
-    mission.id
-  );
-
-  if (missionSubmit !== null)
-    return (
-      <details className="mission-submit">
-        <summary className="mission-submit-summary"></summary>
-        <MissionSubmitEditor
-          learningId={learningId}
-          curriculumId={curriculumId}
-          missionId={mission.id}
-          missionSubmit={missionSubmit}
-          isCreate={false}
-        />
-        <MissionSubmitComment
-          missionId={missionSubmit.missionId}
-          usrId={missionSubmit.usrId}
-        />
-      </details>
+  try {
+    const getArticleSubmitResponse = await fetchWithRetry(
+      `/learning/${learningId}/${curriculumId}/article/${articleId}/submit/user`,
+      {
+        headers: {
+          cookie: cookieStore,
+        },
+      }
     );
 
-  const newMission = await getMission(learningId, curriculumId, mission.id);
+    if (!getArticleSubmitResponse.ok)
+      if (getArticleSubmitResponse.status === 404) return null;
+      else throw new Error("아티클 제출을 조회하는 중 에러가 발생했습니다.");
 
-  return (
-    <details className="mission-submit">
-      <summary className="mission-submit-summary"></summary>
-      <MissionSubmitEditor
-        learningId={learningId}
-        curriculumId={curriculumId}
-        missionId={mission.id}
-        missionSubmit={newMission.missionSubmitForm}
-        isCreate={true}
-      />
-    </details>
-  );
+    return await getArticleSubmitResponse.json();
+  } catch (error) {
+    if (error instanceof UnAuthorizedError) return null;
+    else throw new Error(error.message)
+  }
 }
 
-async function MissionSummary({ learningId, curriculumId, mission }) {
+async function MissionSubmit({ learningId, curriculumId, missionId }) {
   const missionSubmit = await getMissinoSubmit(
     learningId,
     curriculumId,
-    mission.id
+    missionId
   );
 
-  if (missionSubmit === null)
-    return <p className="curriculum-content-status">미제출</p>;
+  if (missionSubmit === null) return <div className="frame-97 background-gray-06 color-gray-04 border-gray-04">{ko_kr.NO_COMPLETE}</div>;
+  if (missionSubmit.status === "SUBMIT") return <div className="frame-97 background-purple-04 color-purple-01">{ko_kr[missionSubmit.status]}</div>;
 
-  return (
-    <p className="curriculum-content-status">
-      {MISSIONSUBMIT_STATUS[missionSubmit.status]}
-    </p>
-  );
+  return <div className="frame-97 background-purple-01 color-white">{ko_kr[missionSubmit.status]}</div>
 }
 
-async function Mission({ learningId, curriculumId, mission }) {
-  return (
-    <details className="curriculum-content-viewer">
-      <summary className="curriculum-content-summary">
-        <div className="space-between">
-          <p className="curriculum-content-summary-title">
-            [미션] {mission.title}
-          </p>
-          <MissionSummary
-            learningId={learningId}
-            curriculumId={curriculumId}
-            mission={mission}
-          />
-        </div>
-      </summary>
-      <div className="flex-end margin1">
-        <MissionSummary
-          learningId={learningId}
-          curriculumId={curriculumId}
-          mission={mission}
-        />
-      </div>
-      <h4 className="curriculum-content-title">{mission.title}</h4>
-      <section className="viewer-content">
-        <Viewer content={mission.description} height="100%" />
-      </section>
-      <MissionSubmit
-        learningId={learningId}
-        curriculumId={curriculumId}
-        mission={mission}
-      />
-    </details>
-  );
-}
+async function ArticleSubmit({ learningId, curriculumId, articleId }) {
+  const articleSubmit = await getArticleSubmit(learningId, curriculumId, articleId);
 
-async function Article({ learningId, curriculumId, article }) {
-  const articleSubmit = await getArticleSubmit(
-    learningId,
-    curriculumId,
-    article.id
-  );
-
-  const articleSubmitStatus =
-    articleSubmit === null
-      ? ARTICLESUBMIT_STATUS.NO_COMPLETE
-      : ARTICLESUBMIT_STATUS[articleSubmit.status];
-
-  return (
-    <details className="curriculum-content-viewer">
-      <summary className="curriculum-content-summary">
-        <div className="space-between">
-          <p className="curriculum-content-summary-title">
-            [아티클] {article.title}
-          </p>
-          <p className="curriculum-content-status">{articleSubmitStatus}</p>
-        </div>
-      </summary>
-      <div className="flex-end margin1">
-        <p className="curriculum-content-status">{articleSubmitStatus}</p>
-      </div>
-      <h4 className="curriculum-content-title">{article.title}</h4>
-      <section className="viewer-content">
-        <Viewer content={article.description} height="100%" />
-      </section>
-      <div className="center">
-        <ArticleSubmitbutton
-          learningId={learningId}
-          curriculumId={curriculumId}
-          articleId={article.id}
-        />
-      </div>
-    </details>
-  );
+  if (articleSubmit === null) return <div className="frame-97 background-gray-06 color-gray-04 border-gray-04">{ko_kr.NO_COMPLETE}</div>;
+  return <div className="frame-97 background-purple-01 color-white">{ko_kr[articleSubmit.status]}</div>
 }
 
 async function CurriculumContentList({ learningId, curriculumId }) {
@@ -291,41 +110,37 @@ async function CurriculumContentList({ learningId, curriculumId }) {
   if (contents.length === 0) return null;
 
   return (
-    <article>
+    <>
       {contents.map((content) => (
-        <section key={content.id}>
-          {content.type === "MISSION" ? (
-            <Link
-              className="link"
-              href={`/participation/${learningId}/${curriculumId}/mission/${content.id}`}
-            >
-              <span className="h4-20">미션</span>
-              <span className="h4-20">{content.title}</span>
-              <span className="caption-12 color-purple-01">참여이전</span>
-            </Link>
-          ) : (
-            /* <Mission
-              learningId={learningId}
-              curriculumId={curriculumId}
-              mission={content}
-            /> */
-            /* <Article
-              learningId={learningId}
-              curriculumId={curriculumId}
-              article={content}
-            /> */
-            <Link
-              className="link"
-              href={`/participation/${learningId}/${curriculumId}/article/${content.id}`}
-            >
-              <span className="h4-20">아티클</span>
-              <span className="h4-20">{content.title}</span>
-              <span className="caption-12 color-purple-01">참여이전</span>
-            </Link>
-          )}
+        <section className="frame-118" key={content.id}>
+          <div className="frame-34-3">
+            <div className="frame-9-2">
+              <div className="frame-93-2">
+                <h4 className="h4-20">{content.type === "MISSION" ? "미션" : "아티클"}</h4>
+              </div>
+            </div>
+          </div>
+          <div className="frame-100-2">
+            <div className="frame-93-3">
+              <div className="frame-4 background-white border-purple-01">
+                <p className="caption-12 color-purple-01">수강 마감 미설정</p>
+              </div>
+              <Link className="h4-20 link" href={content.type === "MISSION" ?
+                `/participation/${learningId}/${curriculumId}/mission/${content.id}` :
+                `/participation/${learningId}/${curriculumId}/article/${content.id}`
+              }>
+                {content.title}
+              </Link>
+            </div>
+          </div>
+          <div className="frame-101-2">
+            {content.type === "MISSION" ?
+              <MissionSubmit learningId={learningId} curriculumId={curriculumId} missionId={content.id} /> :
+              <ArticleSubmit learningId={learningId} curriculumId={curriculumId} articleId={content.id} />}
+          </div>
         </section>
       ))}
-    </article>
+    </>
   );
 }
 
@@ -335,26 +150,36 @@ async function CurriculumList({ learningId }) {
   if (curriculums.length === 0) return null;
 
   return (
-    <section>
-      <h2 className="header2">커리큘럼 목록</h2>
+    <article className="frame">
+      <div className="frame-101">
+        <div className="frame-9">
+          <h2 className="h4-20 color-black">
+            커리큘럼 목록
+          </h2>
+        </div>
+      </div>
+      <div className="line border-gray-05" />
       {curriculums.map((curriculum) => (
         <Fragment key={curriculum.curriculumId}>
-          <details
-            className="viewer-sub"
-            key={curriculum.curriculumId}
-            open={true}
-          >
-            <summary className="viewer-sub-summary h4-20 color-purple-01">
-              {curriculum.title}
-            </summary>
-            <CurriculumContentList
-              learningId={learningId}
-              curriculumId={curriculum.curriculumId}
-            />
-          </details>
+          <div className="frame-119">
+            <div className="frame-123">
+              <div className="frame-34-2">
+                <div className="frame-9-2">
+                  <div className="frame-93-2">
+                    <h3 className="h4-20 color-purple-01">{curriculum.title}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="line border-gray-02" />
+          <CurriculumContentList
+            learningId={learningId}
+            curriculumId={curriculum.curriculumId}
+          />
         </Fragment>
       ))}
-    </section>
+    </article>
   );
 }
 
@@ -363,11 +188,23 @@ async function LearningContent({ learningId }) {
 
   return (
     <div className="wrapper">
-      <h1 className="viewer-title">{learning.title}</h1>
-      <section className="viewer-content">
-        <Viewer content={learning.description} height="100%" />
-      </section>
-      <CurriculumList learningId={learningId} />
+      <div className="frame-93">
+        <div className="frame-57">
+          <h1 className="h3-24 color-black">학습</h1>
+        </div>
+        <article className="frame background-white border-gray-06">
+          <div className="frame-101">
+            <div className="frame-9">
+              <h2 className="h4-20 color-black">{learning.title}</h2>
+            </div>
+          </div>
+          <div className="line border-gray-05" />
+          <section className="frame-120">
+            <Viewer content={learning.description} height="100%" />
+          </section>
+        </article>
+        <CurriculumList learningId={learningId} />
+      </div>
     </div>
   );
 }
--- provider/langs/ko_kr.js
+++ provider/langs/ko_kr.js
@@ -2,4 +2,8 @@ export default {
   CONTENT: "콘텐츠",
   ANNOUNCEMENT: "공지사항",
   BANNER: "배너",
+  COMPLETE: "완료",
+  NO_COMPLETE: "미완료",
+  SUBMIT: "제출됨",
+  APPROVE: "승인됨",
 };
