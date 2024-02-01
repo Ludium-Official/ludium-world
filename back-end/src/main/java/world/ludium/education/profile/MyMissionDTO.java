4287bb63021888a4c463a38d2cf121f6cc3fa848
junhojava <junho9341@gmail.com>
2024-02-01 19:29:39 +0900

    프로필 페이지의 미션 링크 주소 변경

--- back-end/src/main/java/world/ludium/education/profile/MyMissionDTO.java
+++ back-end/src/main/java/world/ludium/education/profile/MyMissionDTO.java
@@ -3,6 +3,6 @@ package world.ludium.education.profile;
 import java.sql.Timestamp;
 import java.util.UUID;
 
-public record MyMissionDTO(UUID missionId, String title, Timestamp createAt, String status, UUID postingId) {
+public record MyMissionDTO(UUID missionId, String title, Timestamp createAt, String status, UUID postingId, UUID curriculumId) {
 
 }
