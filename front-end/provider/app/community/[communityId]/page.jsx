3cfa0e8c4650cc0dc44395788bdf8fbf94949456
IDKNWHORU <junho9341@gmail.com>
2024-02-04 21:09:17 +0900

    콘텐츠 페이지 스타일 추가

--- front-end/provider/app/community/[communityId]/page.jsx
+++ front-end/provider/app/community/[communityId]/page.jsx
@@ -1,12 +1,15 @@
 import BackButton from "@/components/BackButton";
-import ContentNavigation from "@/components/ContentNavigation";
+import Icon from "@/components/Icon";
 import UserNick from "@/components/UserNick";
+import COMMUNITY_TYPE from "@/enums/COMMUNITY_TYPE";
+import UnAuthorizedError from "@/errors/UnAuthorizedError";
 import fetchWithRetry from "@/functions/api";
 import { getTimeStamp } from "@/functions/helper";
 import dynamic from "next/dynamic";
-import ContentCommentEditor from "./ContentCommentEditor";
+import { cookies } from "next/headers";
 import Link from "next/link";
-import COMMUNITY_TYPE from "@/enums/COMMUNITY_TYPE";
+import { Fragment } from "react";
+import ContentCommentEditor from "./ContentCommentEditor";
 import DeleteContentButton from "./DeleteContentButton";
 const Viewer = dynamic(() => import("@/components/Viewer"), { ssr: false });
 
@@ -40,70 +43,134 @@ async function getContentCoomentList(contentId) {
   return await getContentCommentListResponse.json();
 }
 
+async function getProfile() {
+  const cookieStore = cookies();
+
+  try {
+    const getProfileResponse = await fetchWithRetry(`/profile`, {
+      headers: {
+        cookie: cookieStore,
+      },
+    });
+
+    if (!getProfileResponse.ok)
+      throw new Error("프로필을 불러오는 중 에러가 발생했습니다.");
+
+    return await getProfileResponse.json();
+  } catch (error) {
+    if (error instanceof UnAuthorizedError) return null;
+    else throw new Error(error.message);
+  }
+}
+
 async function ContentCoomentList({ contentId }) {
-  const contentCommentList = await getContentCoomentList(contentId);
+  const comments = await getContentCoomentList(contentId);
 
   return (
-    <>
-      <h2 className="header2">댓글 목록</h2>
-      <article className="comment-list">
-        {contentCommentList.map((contentComment) => (
-          <section
-            className="comment"
-            key={`${contentComment.missionId} ${contentComment.usrId} ${contentComment.createAt}`}
-          >
-            <section className="space-between margin1">
-              <p className="text1">
-                작성자: <UserNick usrId={contentComment.usrId} />
-              </p>
-              <p className="text1">{getTimeStamp(contentComment.createAt)}</p>
+    <div className="frame background-white border-gray-06 comment">
+      <div className="frame-101">
+        <div className="frame-9">
+          <h2 className="h4-20 color-black">코멘트</h2>
+        </div>
+      </div>
+      <div className="frame-143">
+        {comments.map((contentComment, index) => (
+          <Fragment key={contentComment.contentId}>
+            <section className="frame-142">
+              <div className="frame-140">
+                <div className="frame-10">
+                  <div className="frame-141">
+                    <h3 className="h4-18">
+                      <UserNick usrId={contentComment.usrId} />
+                    </h3>
+                    <div className="frame-9-3">
+                      <p className="caption-12">
+                        {getTimeStamp(contentComment.createAt)}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="comment-viewer background-white">
+                <Viewer content={contentComment.description} height="100%" />
+              </div>
             </section>
-            <div className="comment-content">
-              <Viewer content={contentComment.description} height="100%" />
-            </div>
-          </section>
+            {index < comments.length - 1 ? (
+              <div className="line border-gray-05" />
+            ) : null}
+          </Fragment>
         ))}
-      </article>
-    </>
+      </div>
+    </div>
   );
 }
 
 export default async function ContentPage({ params: { communityId } }) {
   const content = await getContent(communityId);
+  const profile = await getProfile();
 
   return (
     <>
       <header className="nb">
         <BackButton />
+        <Link className="frame-56 link" href={`/community/${communityId}/edit`}>
+          <Icon src="/icon_write.svg" alt="수정하기" width={24} height={24} />
+          <p className="h4-20 color-purple-01">수정하기</p>
+        </Link>
       </header>
-      <div className="wrapper">
-        <div className="flex-end">
-          <ContentNavigation links={[]}>
-            <DeleteContentButton communityId={communityId} />
-            <Link href={`/community/${communityId}/edit`}>
-              수정 페이지로 이동
-            </Link>
-          </ContentNavigation>
+      <article className="wrapper">
+        <div className="frame-151">
+          <div
+            className={`frame-149 ${
+              content.type === COMMUNITY_TYPE.CONTENT
+                ? "comment-included-left"
+                : ""
+            }`}
+          >
+            <div className="frame background-white border-gray-06">
+              <div className="frame-101">
+                <div className="frame-9">
+                  <div className="frame-145">
+                    <h1 className="h4-20 color-black">{content.title}</h1>
+                  </div>
+                  <div className="frame-9-3">
+                    <p className="caption-12 color-gray-04">
+                      <UserNick usrId={content.usrId} />
+                    </p>
+                    <p className="caption-12 color-gray-04">
+                      {getTimeStamp(content.createAt)}
+                    </p>
+                    {content.type === COMMUNITY_TYPE.CONTENT ? (
+                      profile === null ? null : (
+                        <DeleteContentButton communityId={communityId} />
+                      )
+                    ) : null}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="frame background-white border-gray-06">
+              <div className="frame-101 content-viewer">
+                <Viewer content={content.description} height="100%" />
+              </div>
+            </div>
+          </div>
+          {content.type === COMMUNITY_TYPE.CONTENT ? (
+            <div className="frame-150">
+              <ContentCoomentList contentId={communityId} />
+              <div className="frame background-white border-gray-06 mission-comment mission-comment-editor">
+                <div className="frame-148">
+                  <h2 className="h5-18">코멘트 작성하기</h2>
+                </div>
+                <ContentCommentEditor
+                  contentId={communityId}
+                  usrId={profile === null ? null : profile.id}
+                />
+              </div>
+            </div>
+          ) : null}
         </div>
-        <div className="space-between">
-          <p className="text1">
-            작성자: <UserNick usrId={content.usrId} />
-          </p>
-          <p className="text1">{getTimeStamp(content.createAt)}</p>
-        </div>
-        <h1 type="text" className="viewer-title">
-          {content.title}
-        </h1>
-        <div className="viewer-content">
-          <Viewer content={content.description} height="100%" />
-        </div>
-        {content.type === COMMUNITY_TYPE.CONTENT ? (
-          <>
-            <ContentCoomentList contentId={communityId} />
-            <ContentCommentEditor contentId={communityId} />
-          </>
-        ) : null}
-      </div>
+      </article>
     </>
   );
 }
