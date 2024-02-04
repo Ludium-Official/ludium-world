da7b425c7400f0786ff1c4af75f699edfb05e2e8
IDKNWHORU <junho9341@gmail.com>
2024-02-04 14:40:27 +0900

    콘텐츠 생성 페이지에 스타일 추가

--- front-end/provider/app/community/new/page.jsx
+++ front-end/provider/app/community/new/page.jsx
@@ -11,7 +11,18 @@ export default async function NewContentPage() {
       <header className="nb">
         <BackButton />
       </header>
-      <NewCommunityForm />
+      <article className="wrapper">
+        <div className="frame-93">
+          <div className="frame-57">
+            <h1 className="h3-24 color-black">콘텐츠 작성</h1>
+          </div>
+          <div className="frame-34-4 background-white">
+            <div className="frame-117">
+              <NewCommunityForm />
+            </div>
+          </div>
+        </div>
+      </article>
     </>
   );
 }
