5fffe1c7d26877ab9726b68e163012c26a6c4add
IDKNWHORU <junho9341@gmail.com>
2024-02-08 14:43:55 +0900

    나의 미션 링크 이동 기능 추가

--- front-end/provider/components/profile/Mission.jsx
+++ front-end/provider/components/profile/Mission.jsx
@@ -20,11 +20,12 @@ export default async function Mission({ usrId }) {
   const missions = await getMissionList(usrId);
 
   return (
-    <div className="frame-44-2">
-      <div className="frame-34-7 background-white border-gray-06">
-        <div className="frame-35-2">
-          <h1 className="h4-20 color-black">나의 미션</h1>
-          {/* <Link className="frame-56-2 link" href="/">
+    // <div className="frame-44-2">
+    <div className="frame-34-6 background-white border-gray-06">
+      {/* <div className="frame-34-7 background-white border-gray-06"> */}
+      <div className="frame-35-2">
+        <h1 className="h4-20 color-black">나의 미션</h1>
+        {/* <Link className="frame-56-2 link" href="/">
             <p className="more color-gray-04">모두 보기</p>
             <div className="arrow-right">
               <div className="frame-78">
@@ -37,25 +38,28 @@ export default async function Mission({ usrId }) {
               </div>
             </div>
           </Link> */}
-        </div>
-        {missions.map((mission, index) => (
-          <Fragment key={mission.missionId}>
-            <div className="frame-40">
-              <div className="frame-3 background-white border-purple-01">
-                <p className="caption-12 color-purple-01">
-                  {ko_kr[mission.status]}
-                </p>
-              </div>
-              <Link className="link" href="/">
-                <h2 className="h4-20 color-gray-02">{mission.title}</h2>
-              </Link>
-            </div>
-            {index < missions.length - 1 ? (
-              <div className="line border-gray-05" />
-            ) : null}
-          </Fragment>
-        ))}
       </div>
+      {missions.map((mission, index) => (
+        <Fragment key={mission.missionId}>
+          <div className="frame-40">
+            <div className="frame-3 background-white border-purple-01">
+              <p className="caption-12 color-purple-01">
+                {ko_kr[mission.status]}
+              </p>
+            </div>
+            <Link
+              className="link"
+              href={`/participation/${mission.postingId}/${mission.curriculumId}/mission/${mission.missionId}`}
+            >
+              <h2 className="h4-20 color-gray-02">{mission.title}</h2>
+            </Link>
+          </div>
+          {index < missions.length - 1 ? (
+            <div className="line border-gray-05" />
+          ) : null}
+        </Fragment>
+      ))}
     </div>
+    // </div>
   );
 }
