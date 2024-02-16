a39a83b5f6fca51c0ee0d73bbc6ca1619bc1ec97
IDKNWHORU <junho9341@gmail.com>
2024-02-16 21:05:01 +0900

    작업물 페이지 추가

--- provider/app/work/[workId]/WorkContentEditor.jsx
+++ provider/app/work/[workId]/WorkContentEditor.jsx
@@ -5,8 +5,6 @@ import fetchWithRetry from "@/functions/api";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
-import workstyle from "../work.module.css";
-import WORK_CONTENT_STATUS from "@/enums/WORK_CONTENT_STATUS";
 
 const Editor = dynamic(() => import("@/components/Editor"), {
   ssr: false,
@@ -16,12 +14,11 @@ export default function WorkContentEditor({ detailContent }) {
   const router = useRouter();
   const editorRef = useRef();
   const formRef = useRef();
-  const [pending1, setPending1] = useState(false);
-  const [pending2, setPending2] = useState(false);
+  const [pending, setPending] = useState(false);
 
   const handleEditWorkContent = async (e) => {
     e.preventDefault();
-    setPending1(true);
+    setPending(true);
     const { editorInstance } = editorRef.current;
 
     const updateWorkContentResponse = await fetchWithRetry(
@@ -36,7 +33,7 @@ export default function WorkContentEditor({ detailContent }) {
       }
     );
 
-    setPending1(false);
+    setPending(false);
     if (!updateWorkContentResponse.ok) {
       switch (updateWorkContentResponse.status) {
         case 403:
@@ -54,77 +51,44 @@ export default function WorkContentEditor({ detailContent }) {
     }
   };
 
-  const handleSubmitWorkContent = async () => {
-    setPending2(true);
-    const { editorInstance } = editorRef.current;
-
-    const submitWorkContentResponse = await fetchWithRetry(
-      `/detailed-announcement/${detailContent.detailId}/${detailContent.detailContentId}`,
-      {
-        method: HTTP_METHOD.PUT,
-        body: JSON.stringify({
-          ...detailContent,
-          title: formRef.current.title.value,
-          description: editorInstance.getMarkdown(),
-          status: WORK_CONTENT_STATUS.SUBMIT,
-        }),
-      }
-    );
-
-    setPending2(false);
-    if (!submitWorkContentResponse.ok) {
-      switch (submitWorkContentResponse.status) {
-        case 403:
-        case 404: {
-          const { message } = await submitWorkContentResponse.json();
-          alert(message);
-          break;
-        }
-        default:
-          alert("작업물을 제출하는 중에 에러가 발생했습니다.");
-      }
-    } else {
-      alert("작업물이 제출되었습니다.");
-      router.refresh();
-    }
+  const handleIgnoreEnterKeyDown = (keydownEvent) => {
+    if (keydownEvent.key === "Enter") keydownEvent.preventDefault();
   };
 
   return (
-    <form ref={formRef} onSubmit={handleEditWorkContent}>
-      <div className={workstyle["work-edit-reference-button-area"]}>
-        <button
-          type="button"
-          onClick={handleSubmitWorkContent}
-          disabled={pending2}
-        >
-          {pending2 ? "작업물을 제출하는 중입니다..." : "작업물 제출하기"}
-        </button>
-        <button disabled={pending1}>
-          {pending1 ? "작업물을 수정하는 중입니다..." : "작업물 수정하기"}
-        </button>
+    <form className="frame-116" ref={formRef} onSubmit={handleEditWorkContent}>
+      <div className="input-2">
+        <label className="h5-18 color-gray-03" htmlFor="title">
+          제목
+        </label>
+        <input
+          className="frame-102-3 background-white border-gray-05 p1-18 color-gray-04"
+          type="text"
+          placeholder="제목을 입력해주세요"
+          name="title"
+          id="title"
+          defaultValue={detailContent.title}
+          onKeyDown={handleIgnoreEnterKeyDown}
+        />
       </div>
-      <details className={workstyle["work-edit-wrapper"]} open={true}>
-        <summary>작업물 펼치기 / 접기</summary>
-        <p>
-          작업물 상태: {detailContent.status === "SUBMIT" ? "제출됨" : "생성됨"}
-        </p>
-        <div className={workstyle["work-edit-header-area"]}>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            defaultValue={detailContent.title}
-            placeholder="작업물 제목을 입력해주세요"
-          />
-        </div>
-        <div className={workstyle["work-edit-content-area"]}>
+      <div className="input-2">
+        <p className="h5-18 color-gray-03">내용</p>
+        <div className="frame-102-4 background-white content-editor">
           <Editor
             editorRef={editorRef}
             content={detailContent.description}
-            height={"100%"}
+            height="100%"
           />
         </div>
-      </details>
+      </div>
+      <div className="frame-157">
+        <button
+          className="button-L-2 background-purple-01 h5-18 color-white"
+          disabled={pending}
+        >
+          {pending ? "저장하는 중입니다..." : "저장하기"}
+        </button>
+      </div>
     </form>
   );
 }
