ea08f8c43dca735294a268c241fb05baf85eefa2
IDKNWHORU <junho9341@gmail.com>
2024-02-18 19:18:24 +0900

    sitemap.xml을 생성하는 기능 추가

---
+++ front-end/provider/app/sitemap.js
@@ -0,0 +1,36 @@
+export default function sitemap() {
+  const siteMapUrl = process.env.NEXT_PUBLIC_SITE_MAP_URL;
+
+  if (siteMapUrl === "" || siteMapUrl == null) return [];
+
+  return [
+    {
+      url: `${siteMapUrl}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${siteMapUrl}/announcement`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${siteMapUrl}/work`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+    },
+    {
+      url: `${siteMapUrl}/participation`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${siteMapUrl}/profile`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+    },
+  ];
+}
