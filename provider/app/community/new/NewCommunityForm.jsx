c6215eabd6cf8551151671baa598965344394010
IDKNWHORU <junho9341@gmail.com>
2024-02-22 14:44:36 +0900

    게시글 타입 [자유, 질문]을 생성하는 기능 추가

--- provider/app/community/new/NewCommunityForm.jsx
+++ provider/app/community/new/NewCommunityForm.jsx
@@ -1,7 +1,9 @@
 "use client";
 
+import COMMUNITY_TYPE from "@/enums/COMMUNITY_TYPE";
 import HTTP_METHOD from "@/enums/HTTP_METHOD";
 import fetchWithRetry from "@/functions/api";
+import ko_kr from "@/langs/ko_kr";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
@@ -19,13 +21,16 @@ export default function NewCommunityForm() {
 
     const { editorInstance } = editorRef.current;
 
-    const createContentResponse = await fetchWithRetry(`/content`, {
-      method: HTTP_METHOD.POST,
-      body: JSON.stringify({
-        title: e.target.title.value,
-        description: editorInstance.getMarkdown(),
-      }),
-    });
+    const createContentResponse = await fetchWithRetry(
+      `/content?type=${e.target.type.value}`,
+      {
+        method: HTTP_METHOD.POST,
+        body: JSON.stringify({
+          title: e.target.title.value,
+          description: editorInstance.getMarkdown(),
+        }),
+      }
+    );
 
     setPending(false);
 
@@ -57,6 +62,20 @@ export default function NewCommunityForm() {
           id="title"
           onKeyDown={handleIgnoreEnterKeyDown}
         />
+      </div>
+      <div className="input-2">
+        <label className="h5-18 color-gray-03" htmlFor="type">
+          유형
+        </label>
+        <select
+          className="frame-102-3 background-white border-gray-05 p1-18 color-gray-04"
+          name="type"
+          id="type"
+        >
+          <option value={COMMUNITY_TYPE.CONTENT}>{ko_kr.CONTENT}</option>
+          <option value={COMMUNITY_TYPE.QUESTION}>{ko_kr.QUESTION}</option>
+          <option value={COMMUNITY_TYPE.FREE}>{ko_kr.FREE}</option>
+        </select>
       </div>
       <div className="input-2">
         <p className="h5-18 color-gray-03">내용</p>
