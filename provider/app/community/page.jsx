ef7efe6f5be9cdd0c12171186ebb762e8648d85c
IDKNWHORU <junho9341@gmail.com>
2024-02-24 19:18:49 +0900

    커뮤니티 목록 메타 설명 추가

--- provider/app/community/page.jsx
+++ provider/app/community/page.jsx
@@ -8,7 +8,27 @@ import ko_kr from "@/langs/ko_kr";
 import Link from "next/link";
 
 export const metadata = {
-  title: "콘텐츠 목록",
+  metadataBase: process.env.NEXT_PUBLIC_SITE_MAP_URL,
+  title: "커뮤니티 게시판",
+  description:
+    "웹 3.0 기반 커뮤니티에서 평등한 소통과 창조적 교류를 추구하는 모든 이의 공간. 공지, 콘텐츠, 자유 질문 게시판 이용 가능.",
+  openGraph: {
+    title: "커뮤니티 게시판",
+    description:
+      "웹 3.0 기반 커뮤니티에서 평등한 소통과 창조적 교류를 추구하는 모든 이의 공간. 공지, 콘텐츠, 자유 질문 게시판 이용 가능.",
+    url: process.env.NEXT_PUBLIC_SITE_MAP_URL,
+    siteName: "루디움",
+    locale: "ko_KR",
+    type: "website",
+    images: [
+      {
+        url: "logo1.png",
+        width: 70,
+        height: 32,
+        alt: "루디움",
+      },
+    ],
+  },
 };
 
 async function getContents() {
