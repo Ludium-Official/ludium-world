2ed29b1c828a212ec9ecda9232c8c31200fb676c
junhojava <junho9341@gmail.com>
2024-02-26 23:04:18 +0900

    미션 제출 후 미션 상태를 표시하는 기능 추가

--- provider/app/participation/[participationId]/[curriculmId]/mission/[missionId]/page.jsx
+++ provider/app/participation/[participationId]/[curriculmId]/mission/[missionId]/page.jsx
@@ -1,7 +1,9 @@
 import BackButton from "@/components/BackButton";
 import Icon from "@/components/Icon";
 import fetchWithRetry from "@/functions/api";
+import ko_kr from "@/langs/ko_kr";
 import dynamic from "next/dynamic";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
 const Viewer = dynamic(() => import("@/components/Viewer"), { ssr: false });
@@ -53,10 +55,39 @@ async function getMission(missionId) {
   return await getMissionResponse.json();
 }
 
+async function getMissinoSubmit(learningId, curriculumId, missionId) {
+  const cookieStore = cookies();
+
+  try {
+    const getMissionSubmitResponse = await fetchWithRetry(
+      `/learning/${learningId}/${curriculumId}/mission/${missionId}/submit/user`,
+      {
+        headers: {
+          cookie: cookieStore,
+        },
+      }
+    );
+
+    if (!getMissionSubmitResponse.ok)
+      if (getMissionSubmitResponse.status === 404) return null;
+      else throw new Error("미션 제출을 조회하는 중 에러가 발생했습니다.");
+
+    return await getMissionSubmitResponse.json();
+  } catch (error) {
+    if (error instanceof UnAuthorizedError) return null;
+    else throw new Error(error.message);
+  }
+}
+
 export default async function MissionPage({
   params: { participationId, curriculmId, missionId },
 }) {
   const mission = await getMission(missionId);
+  const missionSubmit = await getMissinoSubmit(
+    participationId,
+    curriculmId,
+    missionId
+  );
 
   return (
     <>
@@ -88,7 +119,20 @@ export default async function MissionPage({
                       width={24}
                       height={24}
                     />
-                    <h1 className="h4-20 color-black">{mission.title}</h1>
+                    <h4 className="h4-20 color-black">{mission.title}</h4>
+                  </div>
+                  <div className="frame-9-3">
+                    <p
+                      className={`caption-12 ${
+                        missionSubmit === null
+                          ? "color-gray-04"
+                          : "color-purple-01"
+                      }`}
+                    >
+                      {missionSubmit === null
+                        ? ko_kr.NO_COMPLETE
+                        : ko_kr[missionSubmit.status]}
+                    </p>
                   </div>
                 </div>
               </div>
