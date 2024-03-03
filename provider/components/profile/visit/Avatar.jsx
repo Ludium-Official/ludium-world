f2ede4beb1c9c04a06f07e47c73678d7e20514af
IDKNWHORU <junho9341@gmail.com>
2024-03-03 19:48:34 +0900

    다른 사용자의 프로필을 조회하는 기능 추가

--- provider/components/profile/visit/Avatar.jsx
+++ provider/components/profile/visit/Avatar.jsx
@@ -1,6 +1,5 @@
+import Icon from "@/components/Icon";
 import dynamic from "next/dynamic";
-import Icon from "../Icon";
-import Link from "next/link";
 
 const Viewer = dynamic(() => import("@/components/Viewer"), { ssr: false });
 
@@ -20,36 +19,16 @@ export default function Avatar({ profile }) {
               />
             </div>
             <p className="avatar-greeting">
-              안녕하세요, <span className="avatar-nick">{profile.nick}</span>님
+              안녕하세요, <span className="avatar-nick">{profile.nick}</span>{" "}
+              입니다.
             </p>
           </div>
           <div className="frame-80">
             <div className="p1-18 color-gray-03">
               <Viewer content={profile.selfIntro.split("\n")[0]} />
             </div>
-            <div className="frame-79">
-              <div className="icon-28">
-                <Icon
-                  className="tel"
-                  src="/icon_tel.svg"
-                  alt="tel"
-                  width={18}
-                  height={18}
-                />
-              </div>
-              <p className="p1-18 color-gray-03">{profile.phnNmb}</p>
-            </div>
           </div>
         </div>
-        <Link className="link button-M-a" href="/profile/edit">
-          <Icon
-            src="/icon_cog_wheel.svg"
-            alt="cog wheel"
-            width={18}
-            height={18}
-          />
-          <p className="p5-18 color-white">프로필 수정</p>
-        </Link>
       </div>
     </div>
   );
