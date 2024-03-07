f70b818a12d600644c20409bda79e0c1d6a1b6f1
IDKNWHORU <junho9341@gmail.com>
2024-03-07 16:41:09 +0900

    관리자 페이지에 학습 현황 조회 페이지 추가

---
+++ src/main/java/world/ludium/education/learning/model/LearnMonitorDTO.java
@@ -0,0 +1,10 @@
+package world.ludium.education.learning.model;
+
+import java.util.UUID;
+
+public interface LearnMonitorDTO {
+    String getCurriculum();
+    String getMission();
+    String getNick();
+    UUID getUsrId();
+}
