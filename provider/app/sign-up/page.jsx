262f239578d0519a7eabad0512e3d612b29a8a49
IDKNWHORU <junho9341@gmail.com>
2024-03-07 19:45:02 +0900

    회원 가입 결과 메시지를 나타내는 기능 추가

--- provider/app/sign-up/page.jsx
+++ provider/app/sign-up/page.jsx
@@ -1,7 +1,25 @@
 import SignUp from "./SignUp";
 
 export const metadata = {
-  title: "내 정보 입력",
+  metadataBase: process.env.NEXT_PUBLIC_SITE_MAP_URL,
+  title: "회원가입",
+  description: "루디움 회원이되어 WEB 3.0 활동을 시작해보세요",
+  openGraph: {
+    title: "회원가입",
+    description: "루디움 회원이되어 WEB 3.0 활동을 시작해보세요",
+    url: process.env.NEXT_PUBLIC_SITE_MAP_URL,
+    siteName: "루디움",
+    locale: "ko_KR",
+    type: "website",
+    images: [
+      {
+        url: "logo1.png",
+        width: 70,
+        height: 32,
+        alt: "루디움",
+      },
+    ],
+  },
 };
 
 export default function SignUpPage() {
