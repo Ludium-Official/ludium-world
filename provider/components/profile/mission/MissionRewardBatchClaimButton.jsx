fbec988d701b4be60abeea8f4c77d427fd6771ac
junho <junho9341@gmail.com>
2024-06-15 22:51:28 +0900

    미션 보상 요청 기능 추가
    미션 보상 요청 버튼 활성 조건
    - 검증 상태가 승인됨(필수)
    - 보상 상태가 요청 안됨 혹은 실패함(필수)
    
    미션 보상 요청 중 브라우저를 새로고침 할 경우 서버에 실시간으로 요청 상태를 조회함

---
+++ provider/components/profile/mission/MissionRewardBatchClaimButton.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function MissionRewardBatchClaimButton() {
+  const handleBatchClaim = () => {
+    alert("기능 구현중입니다");
+  };
+
+  return (
+    <button type="button" onClick={handleBatchClaim}>
+      <h4 className={`h4-20`}>일괄 요청</h4>
+    </button>
+  );
+}
