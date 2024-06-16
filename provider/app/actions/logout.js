d9cd5f97949496a1231f86e9fe27c8bf9b92bc63
junho <junho9341@gmail.com>
2024-06-16 15:34:17 +0900

    쿠키 삭제 함수 대신에 쿠키 수명 옵션을 추가 (#10)
    테스트 서버에서 cookies().delete 함수를 호출했을때 쿠키가 제거되지 않아 옵션값을 추가해봄.

--- provider/app/actions/logout.js
+++ provider/app/actions/logout.js
@@ -6,8 +6,23 @@ import { redirect } from "next/navigation";
 export async function logout() {
   const cookieStore = cookies();
 
-  cookieStore.delete("access_token");
-  cookieStore.delete("ggl_id");
+  cookieStore.set("access_token", null, {
+    path: "/",
+    sameSite: "none",
+    httpOnly: true,
+    secure: true,
+    maxAge: 0,
+    domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+  });
+
+  cookieStore.set("ggl_id", null, {
+    path: "/",
+    sameSite: "none",
+    httpOnly: true,
+    secure: true,
+    maxAge: 0,
+    domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+  });
 
   redirect("/");
 }
